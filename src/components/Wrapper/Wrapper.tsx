8d71e4b876202407c9585664e7054064273d000e
Dániel B. Papp <dpapp@odu.edu>
2022-07-26 11:11:23 -0400

    feat: :zap: Removed duplicate BrowserRouter calling
    When we include Wrapper in the Layout it was trying to create duplicate borwser router providers.
    
    No using Wrapper inside of a BrowserRouter will break the page now.

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Content } from '../Content'
 
 export const Wrapper = ({ routes }: WrapperProps): JSX.Element => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				{routes.map((route, index) => (
-					<Route
-						path={route.path}
-						index={route.index}
-						key={index}
-						element={() => {
-							return <Content>{route.element}</Content>
-						}}
-					/>
-				))}
-			</Routes>
-		</BrowserRouter>
+		<Routes>
+			{routes
+				? routes.map((route, index) => (
+						<Route
+							path={route.path}
+							index={route.index}
+							key={index}
+							element={() => {
+								return <Content>{route.element}</Content>
+							}}
+						/>
+				  ))
+				: null}
+		</Routes>
 	)
 }
 
