31017a1f627341928202a866392e51d9c0947551
Dániel B. Papp <dpapp@odu.edu>
2022-08-10 12:31:41 -0400

    feat: :sparkles: Created Form molecule with custom input fields
    Started working on the reusable form component that uses the floating input elements as well as our reusable button components too. Still WIP...

--- src/components/FormElements/Form.tsx
+++ src/components/FormElements/Form.tsx
@@ -1,5 +1,41 @@
 import * as React from 'react'
+import { Button } from '../Button'
 
-export const Form = () => {
-	return <div>Form</div>
+export const Form = ({
+	children,
+	allowPrevious = false,
+	perviousLabel = 'Back',
+	proceedLabel = 'Submit',
+}: FormProps) => {
+	return (
+		<form className="">
+			{children}
+			<div className="flex justify-between items-center">
+				{allowPrevious && (
+					<Button
+						label={perviousLabel}
+						type="reset"
+						primary={false}
+					/>
+				)}
+				<Button label={proceedLabel} type="submit" primary />
+			</div>
+		</form>
+	)
+}
+
+export type FormProps = {
+	children: React.ReactNode | React.ReactNode[]
+	/**
+	 * A boolean that decides weather to show or hide a reset button.
+	 */
+	allowPrevious: boolean
+	/**
+	 * A descriptive label for the previous / back button.
+	 */
+	perviousLabel?: string
+	/**
+	 * A descriptive label for the submit / next button.
+	 */
+	proceedLabel: string
 }
---
+++ src/stories/Form.stories.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react'
+import { Form, Input } from '../components/FormElements'
+
+export default {
+	title: 'Molecules/Form',
+	component: Form,
+	args: {},
+	argTypes: {},
+}
+
+const Template = (args) => {
+	const [formData, setFormData] = React.useState({
+		firstName: '',
+		lastName: '',
+		email: '',
+		password: '',
+		passwordConfirmation: '',
+	})
+	return (
+		<Form {...args}>
+			<div className="flex justify-between gap-5">
+				<Input
+					label="First Name"
+					name="firstName"
+					type="text"
+					onChange={(s) => {
+						setFormData({ ...formData, firstName: s })
+					}}
+					defaultValue={formData.firstName}
+				/>
+				<Input
+					label="Last Name"
+					name="lastName"
+					type="text"
+					onChange={(s) => {
+						setFormData({ ...formData, lastName: s })
+					}}
+					defaultValue={formData.lastName}
+				/>
+			</div>
+			<Input
+				label="Email"
+				name="email"
+				type="email"
+				onChange={(s) => {
+					setFormData({ ...formData, email: s })
+				}}
+				defaultValue={formData.email}
+			/>
+			<div className="flex justify-between gap-5">
+				<Input
+					label="Password"
+					name="password"
+					type="password"
+					onChange={(s) => {
+						setFormData({ ...formData, password: s })
+					}}
+					defaultValue={formData.password}
+				/>
+				<Input
+					label="Password Confirmation"
+					name="passwordConfirmation"
+					type="password"
+					onChange={(s) => {
+						setFormData({
+							...formData,
+							passwordConfirmation: s,
+						})
+					}}
+					defaultValue={formData.passwordConfirmation}
+				/>
+			</div>
+		</Form>
+	)
+}
+export const Default = Template.bind({})
+Default.args = {}
