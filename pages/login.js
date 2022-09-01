8b13e1c9e57948344ee4319f8a6f4ab1dcafd1ed
Joel DeSante <joeldesante@gmail.com>
2022-09-01 18:03:24 -0400

    Scaffold NextJS and add some of the existing code.

---
+++ pages/login.js
@@ -0,0 +1,7 @@
+const Login = () => {
+    return <div>
+        <a href="#">Login with Google</a>
+    </div>
+}
+
+export default Login;
---
+++ pages/users/[user]/index.js
@@ -0,0 +1,5 @@
+const UserProfile = () => {
+    return <div>Profile</div>
+}
+
+export default UserProfile;
