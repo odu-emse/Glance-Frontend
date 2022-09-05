8fa2a9919931e9329e13a45fb291945275bcdd7a
Joel DeSante <joeldesante@gmail.com>
2022-09-05 18:03:22 -0400

    Add basic Profile layout

--- pages/users/[user]/index.js
+++ pages/users/[user]/index.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const UserProfile = () => {
-    return <div>Profile</div>
+    return (
+        <div className="flex flex-auto items-center">
+            <div className="rounded-full w-40 h-40 bg-slate-200 overflow-hidden relative drop-shadow mr-5">
+                <Image src="https://images.unsplash.com/photo-1554727242-741c14fa561c" layout="fill" objectFit="cover" />
+            </div>
+            <div>
+                <h1 className="text-2xl">Jada Daphne</h1>
+                <p>jdaph002</p>
+            </div>
+        </div>
+    );
 }
 
 export default UserProfile;
