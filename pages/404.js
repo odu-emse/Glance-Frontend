a46a199c76fff74bf7ed09746bcbfbcde2328d98
kennydd <kdang004@odu.edu>
2022-09-08 14:24:51 -0400

    Migrated 404 page to NextJS frontend

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const NotFound = () => {
+	return (
+		<div className="mx-auto w-full flex flex-col items-center justify-center h-screen">
+			<h1 className="font-semibold text-7xl">404 - Not Found!</h1>
+			<Link className="underline text-blue-500" href="/">
+				Go Home
+			</Link>
+		</div>
+	)
+}
+
+export default NotFound;
