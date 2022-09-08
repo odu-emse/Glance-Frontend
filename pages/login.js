d2ddf42bc328cedd7a332b746bc34c12d0dc9583
Joel DeSante <joeldesante@gmail.com>
2022-09-08 15:33:13 -0400

    Add the google login button

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,20 @@
+import { useGoogleLogin } from '@react-oauth/google';
+
 const Login = () => {
+
+    const login = useGoogleLogin({
+        onSuccess: async codeResponse => console.log(codeResponse),
+        onError: error => console.log(error),
+        flow: 'auth-code',
+    });
+
     return <div>
-        <a href="#">Login with Google</a>
+        <button 
+            onClick={() => login()}
+            className="p-3 bg-slate-400"
+        >
+            Login with Google
+        </button>
     </div>
 }
 
