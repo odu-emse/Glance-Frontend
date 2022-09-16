caaf7c5f4c0100d4357d2c34047105bb21be65dc
Joel DeSante <joeldesante@gmail.com>
2022-09-16 18:11:37 -0400

    Update content type to request JSON response.

--- pages/api/auth/redirect.js
+++ pages/api/auth/redirect.js
@@ -1,29 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-const authCodeQuery = (code) => {
-  const payload = {
-      query: `{
-          login(code: "${code}")
-      }`
-  }
-
-  return payload
-}
-
 export default async function handler(req, res) {
 
-  const response = {
-    ok: true,
-    status: "OK" 
-  }
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query: `{
+        login(code: "${req.query.code}")
+      }`
+    }),
+    credentials: 'include',
+    cache: 'no-cache',
+  })
   
   if(!response.ok) {
+    console.log(await response.text())
     res.status(400).send("Error");
     return;
   }
 
-  //const body = await response.json();
-  //console.log(body)
-  //res.redirect("/");
-  res.status(200).send("Hello")
+  const body = await response.json();
+  if(body.errors !== undefined) {
+    console.log(body)
+    res.status(400).send("Error");
+    return;
+  }
+
+  res.redirect("/");
 }
