4fb7a79b23a891e3897862580a27767b18d8f35f
Dániel B. Papp <dpapp@odu.edu>
2022-09-21 13:08:41 -0400

    feat(UI): :sparkles: Added a default Layout with sidebar
    Wrapped the whole application in the Layout component so we can control the flow of content from one component

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import Head from 'next/head'
-import '../styles/globals.css'
+import Layout from '@/components/Layout';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
-  return (
-    <GoogleOAuthProvider clientId='179547029989-eueljn9jl5gqgforq4jpav2qddfhqhlu.apps.googleusercontent.com'>
-      <Component {...pageProps} />
-    </GoogleOAuthProvider>
-  );
+	return (
+		<GoogleOAuthProvider clientId="179547029989-eueljn9jl5gqgforq4jpav2qddfhqhlu.apps.googleusercontent.com">
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</GoogleOAuthProvider>
+	);
 }
 
-export default App
+export default App;
