96e9f677231c48821ef35e7ddbe3d288d4dd3726
Dániel B. Papp <dpapp@odu.edu>
2022-09-27 16:22:26 -0400

    fix: :recycle: Changed the data structure of the prop
    I changed the data structure that is being passed in as a prop and also changed the functionality that the component handles such as mapping over dynamic values.

--- src/components/Assignment/AssignmentPanel.tsx
+++ src/components/Assignment/AssignmentPanel.tsx
@@ -1,88 +1,109 @@
 import * as React from 'react'
-import { GrClose} from 'react-icons/gr'
+import { GrClose } from 'react-icons/gr'
 
-export const AssignmentPanel = ({ open }: AssignmentPanelProps): JSX.Element => {
+export const AssignmentPanel = ({
+	open,
+	handle,
+	moduleInformation,
+}: AssignmentPanelProps): JSX.Element => {
+	const heading = 'text-sm font-medium pb-1.5'
 
-const heading = [
-    "text-sm font-medium pb-1.5"
-]
+	const list = 'text-xs list-disc pl-3'
 
-const list = [
-    "text-xs list-disc pl-3"
-]
+	return (
+		<aside
+			id="sidePanel"
+			className={`absolute ${
+				open ? 'right-0' : '-right-full'
+			} top-0 h-screen overflow-y-scroll w-96 bg-white transition-all drop-shadow-lg p-0`}
+		>
+			<button
+				id="exitButton"
+				className="absolute top-0 left-0 pt-3 pl-3 text-white"
+				onClick={() => handle(!open)}
+			>
+				<GrClose />
+			</button>
 
-	return (!open ? (null) : (
-            <aside id="sidePanel" className={`relative w-96 bg-white transition-all drop-shadow-lg p-0`}>
-                
-                <button 
-                        id="exitButton" 
-                        className="absolute top-0 left-0 pt-3 pl-3 text-white text-gray-800"
-                        onClick={()=> setOpen(!open)}>
-                        <GrClose />
-                </button>
-                    
-                <div id="content" className="h-full flex flex-col py-8 px-4 top-0 right-0">
+			<div
+				id="content"
+				className="h-full flex flex-col py-8 px-4 top-0 right-0"
+			>
+				<div
+					id="title"
+					className="flex text-base font-medium justify-center"
+				>
+					<h1>{moduleInformation.moduleTitle}</h1>
+				</div>
 
-                    <div id="title" className="flex text-base font-medium justify-center">
-                        <h1>Introduction to Organizational Analysis</h1>
-                    </div>
+				<div
+					id="subTitle"
+					className="flex pt-1.5 pb-8 text-xs justify-center text-slate-500"
+				>
+					<h6>{moduleInformation.assignmentTitle}</h6>
+				</div>
 
-                    <div id="subTitle" className="flex pt-1.5 pb-8 text-xs justify-center text-slate-500">
-                        <h6>Assignment 4</h6>
-                    </div>
+				<div id="reminders" className="pb-8">
+					<h1 className={heading}>Set Reminder</h1>
+					<ul className="text-slate-500 text-xs">
+						<li>3 days before due</li>
+						<li>1 week before due date</li>
+						<li>24 hours before due date</li>
+					</ul>
+				</div>
 
-                    <div id="reminders" className="pb-8">
-                        <h1 className={heading}>Set Reminder</h1>
-                        <ul className="text-slate-500 text-xs">
-                            <li>3 days before due</li>
-                            <li>1 week before due date</li>
-                            <li>24 hours before due date</li>
-                        </ul>
-                    </div>
+				<div id="software" className="pb-8">
+					<h1 className={heading}>Required Software</h1>
+					<ul className={list}>
+						{moduleInformation.software.map(
+							(software, sfwIndex) => (
+								<li key={sfwIndex}>{software}</li>
+							)
+						)}
+					</ul>
+				</div>
 
-                    <div id="software" className="pb-8">
-                        <h1 className={heading}>Required Software</h1>
-                        <ul className={list}>
-                            <li>Adobe Acrobat / Flash enabled browser</li>
-                            <li>Google Chrome 61+</li>
-                        </ul>
-                    </div>
+				<div id="modules" className="pb-8">
+					<h1 className={heading}>Required Modules</h1>
+					<ul className={list}>
+						{moduleInformation.deps.map((dep, depIndex) => (
+							<li key={depIndex}>{dep}</li>
+						))}
+					</ul>
+				</div>
 
-                    <div id="modules" className="pb-8">
-                        <h1 className={heading}>Required Modules</h1>
-                        <ul className={list}>
-                            <li>ENMA 601 Module 400-489 Completed</li>
-                            <li>ENMA 601 Assignment 1-3 Completed</li>
-                        </ul>
-                    </div>
+				<div id="connections" className="pb-8">
+					<h1 className={heading}>Connections</h1>
+					<ul className="text-xs">
+						{moduleInformation.connections.map((mod, modIndex) => (
+							<li className="pb-1 border-b" key={modIndex}>
+								{mod.moduleTitle} - {mod.assignmentTitle}
+							</li>
+						))}
+					</ul>
+				</div>
 
-                    <div id="connections" className="pb-8">
-                        <h1 className={heading}>Connections</h1>
-                        <ul className="text-xs">
-                            <li className="pb-1">ENMA 601 Module 400-489 - Section 10</li>
-                            <hr />
-                            <li className="py-1">ENMA 601 Module 400-489 - Section 3</li>
-                            <hr />
-                            <li className="py-1">ENMA 601 Module 400-489 - Section 15</li>
-                            <hr />
-                            <li className="py-1">ENMA 601 Module 400-489 - Section 8</li>
-                            <hr />
-                            <li className="py-1">ENMA 601 Module 400-489 - Section 12</li>
-                            <hr />
-                            <li className="py-1">ENMA 601 Module 400-489 - Section 14</li>
-                        </ul>
-                    </div>
-
-                    <button id="startButton" className="left-2 right-2 bottom-4 text-sm text-white uppercase py-1.5 px-4.5 bg-sky-300">
-                        <h1>Start Assignment</h1>
-                    </button>
-
-                    </div>
-            </aside>
-            )
-        )
+				<button
+					id="startButton"
+					className="left-2 right-2 bottom-4 text-sm text-white uppercase py-1.5 px-4.5 bg-sky-300"
+				>
+					<h1>Start Assignment</h1>
+				</button>
+			</div>
+		</aside>
+	)
 }
 
 type AssignmentPanelProps = {
-    open: boolean
+	open: boolean
+	handle: (open: boolean) => void
+	moduleInformation: ModuleInformation
+}
+
+type ModuleInformation = {
+	moduleTitle: string
+	assignmentTitle: string
+	deps?: string[]
+	software?: string[]
+	connections?: ModuleInformation[]
 }
