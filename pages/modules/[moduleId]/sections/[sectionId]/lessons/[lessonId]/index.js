5137b1558c5fdffd1bdcf9448c69f22b5c0f1257
Joel DeSante <joeldesante@gmail.com>
2022-09-28 13:55:40 -0400

    Fix linting errors

--- pages/modules/[moduleId]/sections/[sectionId]/lessons/[lessonId]/index.js
+++ pages/modules/[moduleId]/sections/[sectionId]/lessons/[lessonId]/index.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import ModuleNavigation from '@/components/modules/ModuleSidebar/ModuleNavigation';
