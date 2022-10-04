0de8e4cef297ec17e85bc9bdf6955d1e082f9462
Dániel B. Papp <dpapp@odu.edu>
2022-10-04 14:49:45 -0400

    fix: ♻️ Changed variants we are showing
    Now the variants are named correctly, and are showing actual variation of rendering.

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react'
-import { LessonLink, LessonLinkProps } from '../FormElements/LessonLink'
+import { LessonLink} from '../FormElements/LessonLink'
+import type {LessonLinkProps} from '../FormElements/LessonLink'
 
 export const Accordion = ({
     lessons,
+    title
 } : AccordionProps) =>{
     return(   
         <div className="accordion">
@@ -11,8 +13,8 @@ export const Accordion = ({
             type="button" 
             className="flex items-center justify-between w-80 p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-400 rounded-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 hover:border-gray-100 hover:bg-gray-100 dark:hover:bg-gray-800 bg-gray-400 dark:bg-gray-800 text-black dark:text-white"
             >
-                <span>Topic 1</span>
-                <span>{
+                <span>{title}</span>
+                <span>{lessons?.length ??
                 lessons
                 .filter(status => (status.checked == true)).length
                 }/{lessons.length}</span>
@@ -22,7 +24,7 @@ export const Accordion = ({
                 <div className="p-5 font-light border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900">
                     <p className="mb-2 text-gray-900 dark:text-gray-400">
                         { 
-                        lessons.map((lesson, lessonIndex) => (
+                        lessons?.length && lessons.map((lesson, lessonIndex) => (
                             <LessonLink
                                 key={lessonIndex}
                                 label={lesson.label}
@@ -39,6 +41,7 @@ export const Accordion = ({
 }
 
 export type AccordionProps = {
+    title: string
     lessons: LessonLinkProps[]
 }
 
