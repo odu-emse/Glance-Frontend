1a56a2cc14c5bd11a970d1165c7848bda6eb98f5
Dániel B. Papp <dpapp@odu.edu>
2022-10-18 13:27:02 -0400

    :bug: (routing): Added redirect from home page if logged in

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import LoadingSplash from '@/components/LoadingSplash';
 import Head from 'next/head';
+import useAuth from '@/hooks/useAuth';
+import { useRouter } from 'next/router';
 
 const Index = () => {
+  const router = useRouter();
+  const { isAuthorized, loading } = useAuth();
+  if(loading) return <p>Loading...</p>
+  if(isAuthorized) router.push('/modules')
   return (
     <div className='flex justify-center items-center h-screen' data-testid={"index-head"}>
       <Head>
