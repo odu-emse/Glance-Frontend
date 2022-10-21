e38f0cbebbb26a4389b1cc6dee12a35394ce9b90
Ramagiri Divya Sree <drama001@odu.edu>
2022-10-21 16:03:32 -0400

    updating modules page into responsive page

--- pages/modules/[module]/index.js
+++ pages/modules/[module]/index.js
@@ -3,14 +3,14 @@ const Module = () => {
         <div>
             <div className="mx-auto max-w-7xl py-4 px-4 w-3/4 sm:w-full xl:w-2/3">
                 <div className="flex xl:flex-row flex-col-reverse">
-                    <div className="xl:w-2/3 w-full sm:mb-4 sm:mr-0 lg:mb-0 lg:mr-5">
-                        <h1 className="text-3xl font-bold mb-2">
+                    <div className="w-full xl:w-2/3 lg:w-2/3 md:w-2/3 mt-2 sm:mb-4 sm:mr-0 lg:mb-0 lg:mr-5">
+                        <h1 className="text-xl lg:text-3xl md:text-2xl font-bold mb-2">
                             Module {module.moduleNumber} | {module.moduleName}
                         </h1>
                         <p className="mb-2">{module.intro}</p>
-                        <div className="w-full xl:w-1/2">
+                        <div className="w-full xl:w-1/2 lg:w-1/2 md:1/2 md:w-1/4">
                             <p className="font-light text-yellow-500 flex items-center">
-                                <span className="pr-2">
+                                <span className="pr-2 text-sm lg:text-lg md:text-sm">
                                     100%
                                 </span>
                                 {/* <StarRatingComponent
@@ -32,14 +32,14 @@ const Module = () => {
                                 </span> */}
                             </p>
                         </div>
-                        <p className="mb-4">
+                        <p className="mb-4 text-xs lg:text-md md:text-sm">
                             Instructed by{' '}
                             <a className="underline" href="./">
                                 {module.instructor}
                             </a>
                         </p>
                         <div className="border shadow-md rounded-sm py-3 px-4 bg-gray-50 border-gray-50">
-                            <h3 className="text-lg font-bold">
+                            <h3 className="text-sm lg:text-lg md:text-sm font-bold">
                                 Module objectives
                             </h3>
                             {/*{module.keywords.map((key, index) => (
@@ -55,64 +55,66 @@ const Module = () => {
                             ))}*/}
                         </div>
                         <div className="border shadow-md rounded-sm py-3 px-4 mt-3 bg-gray-50 border-gray-50">
-                            <h3 className="text-lg font-bold">
+                            <h3 className="text-sm lg:text-lg md:text-sm font-bold">
                                 Module description
                             </h3>
                             <p>{module.description}</p>
                         </div>
                         <div className="border shadow-md rounded-sm py-3 px-4 mt-3 bg-gray-50 border-gray-50">
-                            <h3 className="text-lg font-bold">Requirements</h3>
+                            <h3 className="text-sm lg:text-lg md:text-sm font-bold">
+                                Requirements
+                                </h3>
                             <p>{module.description}</p>
                         </div>
                     </div>
-                    <div className="xl:w-1/3 w-full xl:border border-gray-50 px-3 py-4 rounded-sm xl:shadow-md">
+                    <div className="w-full xl:w-1/3 lg:w-1/3 md:w-3/4 border border-gray-50 px-3 py-4 rounded shadow-md lg:text-left">
                         {/*TODO: we need to get content delivery working to get this fixed */}
                         {/* <img
                             src={`${content.href}/story_content/thumbnail.jpg`}
                             alt={`${module.moduleName} module thumbnail`}
                             className="w-full"
                         /> */}
-                        <div className="module--housing--inclusion">
-                            <h5 className="mt-3 text-lg font-bold">
+                        <div className="module--housing--inclusion ">
+                            <h5 className="mt-3 text-md lg:text-lg font-bold">
                                 This module includes:{' '}
                             </h5>
-                            <span className="flex mt-3 font-light items-center">
-                                <div className="text-gray-400 mr-3">
+                            <span className="flex mt-3 font-light items-center text-sm lg:text-md md:text-sm">
+                                <div className="text-gray-400 mr-3 l:text-md md:text-sm sm:text-sm">
                                     {/* <Youtube /> */}
                                 </div>
-                                {module.duration} hours on demand video
+                                {module.duration} Hours on demand video
                             </span>
-                            <span className="flex mt-3 font-light items-center">
+                            <span className="flex mt-3 font-light items-center text-sm lg:text-md md:text-sm">
                                 <div className="text-gray-400 mr-3">
                                     {/* <Book /> */}
                                 </div>
-                                {module.numSlides} interactive slides
+                                {module.numSlides} Interactive slides
                             </span>
-                            <span className="flex mt-3 font-light items-center">
+                            <span className="flex mt-3 font-light items-center text-sm lg:text-md md:text-sm">
                                 <div className="text-gray-400 mr-3">
                                     {/* <Download /> */}
                                 </div>
                                 Downloadable resources
                             </span>
-                            <span className="flex mt-3 font-light items-center">
+                            <span className="flex mt-3 font-light items-center text-sm lg:text-md md:text-sm">
                                 <div className="text-gray-400 mr-3">
                                     {/* <Smartphone /> */}
                                 </div>
                                 Available on mobile devices
                             </span>
-                            <span className="flex mt-3 font-light items-center">
+                            <span className="flex mt-3 font-light items-center text-sm lg:text-md md:text-sm">
                                 <div className="text-gray-400 mr-3">
                                     {/* <Repeat /> */}
                                 </div>
                                 Lifetime access
                             </span>
-                            <span className="flex mt-3 font-light items-center">
-                                <div className="text-gray-400 mr-3">
+                            <span className="flex mt-3 font-light items-center text-sm lg:text-md md:text-sm">
+                                <div className="text-gray-400 mr-3 l:text-md md:text-sm sm:text-sm">
                                     {/* <LifeBuoy /> */}
                                 </div>
                                 On-demand helping professionals
                             </span>
-                            <span className="flex mt-3 font-light items-center">
+                            <span className="flex mt-3 font-light items-center text-sm lg:text-md md:text-sm">
                                 <div className="text-gray-400 mr-3">
                                    {/*} <Award /> */}
                                 </div>
@@ -124,11 +126,11 @@ const Module = () => {
                                 onClick={(e) => {
                                     addModule(module._id)
                                 }}
-                                className="bg-blue-400 rounded text-white py-2 px-4 w-full"
+                                className="bg-blue-400 rounded text-white py-2 px-4 w-5/6 lg:w-full text-sm lg:text-md md:text-sm"
                             >
                                 Enroll module
                             </button>
-                            <button className="border-2 border-blue-300 rounded py-2 px-4 w-full mt-3">
+                            <button className="border-2 border-blue-300 rounded py-2 px-4 w-5/6 lg:w-full mt-3 text-sm lg:text-md md:text-sm">
                                 Favorite
                             </button>
                         </div>
--- pages/users/[user]/index.js
+++ pages/users/[user]/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Sidebar from '../../components/sidebar'
 
 export default function UserProfile(){
 
