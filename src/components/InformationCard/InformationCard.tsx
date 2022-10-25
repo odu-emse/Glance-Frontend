a453e258ad6bbbea8b2e28da32e3ad87f8242766
Dániel B. Papp <dpapp@odu.edu>
2022-10-25 14:35:42 -0400

    🧹 (Information Card): Made some minor changes to formatting I also added the export line to the component's index file to make sure that we are able to call this component when it is ready

--- src/components/InformationCard/InformationCard.tsx
+++ src/components/InformationCard/InformationCard.tsx
@@ -1,128 +1,128 @@
-import * as React from 'react';
-import { FaRegCheckCircle } from 'react-icons/fa';
-import {BsFillExclamationOctagonFill} from 'react-icons/bs';
-import {MdClose} from 'react-icons/md'
-import {BsFillClockFill, BsFillBellFill} from 'react-icons/bs'
-
+import * as React from 'react'
+import { FaRegCheckCircle } from 'react-icons/fa'
+import { BsFillExclamationOctagonFill } from 'react-icons/bs'
+import { MdClose } from 'react-icons/md'
+import { BsFillClockFill, BsFillBellFill } from 'react-icons/bs'
 
 export const InformationCard = ({
-    percentComplete,
-    statusMessage,
-    testType,
-    testModule,
-    moduleIdentifier,
-    dueDate,
-    grade,
-    percentage,
-    handle
+	percentComplete,
+	statusMessage,
+	testType,
+	testModule,
+	moduleIdentifier,
+	dueDate,
+	grade,
+	percentage,
+	handle,
 }: InformationProps): JSX.Element => {
-   return(
-       <div className="border-solid border-gray rounded border flex justify-between w-2/5">
-        {
-        percentComplete && (
-                <>
-        <div className="flex justify-start items-center px-2">
-            
-        <div className="checkIcon">
-            <FaRegCheckCircle fill="green" size="30px"/>
-        </div>
-        <div className="modueleInformation px-2">
-            <p className="mx-2 my-2 text-l">
-                {statusMessage}
-            </p>
-            <p className="mx-2 my-2 text-gray-400 inline-block text-xs">
-                {percentComplete}
-            </p>
-       </div>   
-        </div>
-       <div className="closeIcons flex gap-2 justify-items-end my-10 mx-2">
-            <BsFillExclamationOctagonFill size="15px" fill="gray" onClick={handle}/>
-            <MdClose fill="gray" size="20px" onClick={handle}/>
-       </div>
-    </>
-    )}
+	return (
+		<div className="border-solid border-gray rounded border flex justify-between w-2/5">
+			{percentComplete && (
+				<>
+					<div className="flex justify-start items-center px-2">
+						<div className="checkIcon">
+							<FaRegCheckCircle fill="green" size={30} />
+						</div>
+						<div className="modueleInformation px-2">
+							<p className="mx-2 my-2 text-l">{statusMessage}</p>
+							<p className="mx-2 my-2 text-gray-400 inline-block text-xs">
+								{percentComplete}
+							</p>
+						</div>
+					</div>
+					<div className="closeIcons flex gap-2 justify-items-end my-10 mx-2">
+						<BsFillExclamationOctagonFill
+							size={28}
+							fill="gray"
+							onClick={handle}
+						/>
+						<MdClose fill="gray" size={30} onClick={handle} />
+					</div>
+				</>
+			)}
 
-    {
-    testType && (
-        <>
-        <div className='m-3 flex items-center'>
-            <div className="testInformation">
-            <p>{testType} : {testModule}</p>
-            <p className="mx-2 my-2 text-gray-400 inline-block text-xs">
-                {moduleIdentifier}
-            </p>
-            <div className='flex gap-1 items-center'>
-            <BsFillClockFill size="12px"/> 
-            <p>Opens in {dueDate}</p>
-            </div>
-            </div>
-        </div>
-        <div className="notificationIcon grid justify-items-end my-10 mx-3">
-        <BsFillBellFill size="20px" onClick={handle}/>
-        </div>
-        </>
-    )
-    }
-    {
-        grade && (
-            <>
-                <div className='m-3 flex flex-col'>
-                   <div className="module text-lg font-medium"> 
-                   {moduleIdentifier}  </div>
-                   <div className="flex justify-start items-center text-gray-600 inline-block text-sm">
-                    {percentage}% passing rate </div>
-                   <div className="flex gap-1 items-center text-gray-500 inline-block text-xs"> 
-                   Average Grade: {grade} </div>
-                </div>
-                <div className="moreOptions grid justify-items-end m-2 text-gray-500 inline-block text-sm"
-                onClick={handle}
-                >
-                more
-                </div>
-            </>
-            )
-        
-    }
-    </div>
-   )
+			{testType && (
+				<>
+					<div className="m-3 flex items-center">
+						<div className="testInformation">
+							<p>
+								{testType} : {testModule}
+							</p>
+							<p className="mx-2 my-2 text-gray-400 inline-block text-xs">
+								{moduleIdentifier}
+							</p>
+							<div className="flex gap-1 items-center">
+								<BsFillClockFill size="12px" />
+								<p>Opens in {dueDate}</p>
+							</div>
+						</div>
+					</div>
+					<div className="notificationIcon grid justify-items-end my-10 mx-3">
+						<BsFillBellFill size={30} onClick={handle} />
+					</div>
+				</>
+			)}
+			{grade && (
+				<>
+					<div className="m-3 flex flex-col">
+						<div className="module text-lg font-medium">
+							{moduleIdentifier}{' '}
+						</div>
+						<div className="flex justify-start items-center text-gray-600 inline-block text-sm">
+							{percentage}% passing rate{' '}
+						</div>
+						<div className="flex gap-1 items-center text-gray-500 inline-block text-xs">
+							Average Grade: {grade}{' '}
+						</div>
+					</div>
+					<div
+						className="moreOptions grid justify-items-end m-2 text-gray-500 inline-block text-sm"
+						onClick={handle}
+					>
+						more
+					</div>
+				</>
+			)}
+		</div>
+	)
 }
 
 export type InformationProps = {
-    percentComplete: string,
-    /**
+	percentComplete: string
+	/**
 	 * String that shows up the percentage complete on Default Card
 	 */
-    statusMessage: string,
-    /**
+	statusMessage: string
+	/**
 	 * String that shows up the status message on Default Card
 	 */
-    testType: string,
-    /**
+	testType: string
+	/**
 	 * String that shows up the test type on Primary Card
 	 */
-    testModule: string,
-    /**
+	testModule: string
+	/**
 	 * String that shows up the type of TestModule on Primary Card
 	 */
-    dueDate: string,
-    /**
-	 * String that shows up the dueDate 
+	dueDate: string
+	/**
+	 * String that shows up the dueDate
 	 */
-    moduleIdentifier: string,
-    /**
+	moduleIdentifier: string
+	/**
 	 * String that shows up the Module Identifier
 	 */
-    grade: string,
-    /**
+	grade: string
+	/**
 	 * String that shows up the grade
 	 */
-    percentage: string,
-    /**
+	percentage: string
+	/**
 	 * String that shows up the percentage in the Secondary Card
 	 */
-    handle: ()=> void 
-    /**
-	 * A function placeholder to differentiate the onclick functionality 
-     * based on the usage for different icons.
+	handle: () => void
+	/**
+	 * A function placeholder to differentiate the onclick functionality
+	 * based on the usage for different icons.
 	 */
-    }
+}
--- src/stories/InformationCard.stories.tsx
+++ src/stories/InformationCard.stories.tsx
@@ -1,15 +1,18 @@
-import { InformationCard, InformationProps } from "../components/InformationCard/InformationCard";
+import * as React from 'react'
+import {
+	InformationCard,
+	InformationProps,
+} from '../components/InformationCard'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-export default{
-    title: 'Molecules/Information Card',
-    component: InformationCard,
-    argTypes: {},
+export default {
+	title: 'Molecules/Information Card',
+	component: InformationCard,
+	argTypes: {},
 } as ComponentMeta<typeof InformationCard>
 
-function onHandle(){
-    console.log("works");
-    
+function onHandle() {
+	console.log('works')
 }
 
 const Template: ComponentStory<typeof InformationCard> = (
@@ -19,20 +22,20 @@ const Template: ComponentStory<typeof InformationCard> = (
 export const Default = Template.bind({})
 Default.args = {
 	percentComplete: 'Complete',
-    statusMessage: 'Spend 13 or more hours in module view'
+	statusMessage: 'Spend 13 or more hours in module view',
 }
 
 export const Primary = Template.bind({})
 Primary.args = {
-    testType: 'Assignment',
-    testModule: 'Introduction to Organization Analysis',
-    moduleIdentifier: 'M 137G',
-    dueDate: '27 module hours'
+	testType: 'Assignment',
+	testModule: 'Introduction to Organization Analysis',
+	moduleIdentifier: 'M 137G',
+	dueDate: '27 module hours',
 }
 
 export const Secondary = Template.bind({})
 Secondary.args = {
-    grade: 'A',
-    percentage: '84',
-    moduleIdentifier: 'ENMA 715'
+	grade: 'A',
+	percentage: '84',
+	moduleIdentifier: 'ENMA 715',
 }
