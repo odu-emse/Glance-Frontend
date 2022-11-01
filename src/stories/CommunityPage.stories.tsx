752451a17c2ab0f7fe2d55367a807a1d79c30497
Dániel B. Papp <dpapp@odu.edu>
2022-11-01 12:04:02 -0400

    :recycle: (PR): Modified PR for Community Page organism I made some alterations to the markup and props that are being passed in to fix type errors.

--- src/stories/CommunityPage.stories.tsx
+++ src/stories/CommunityPage.stories.tsx
@@ -3,14 +3,8 @@ import { CommunityPage, CommunityPageProps } from "../components/CommunityPage";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 export default {
-  title: "Molecules/Community Page", //providing path
+  title: "Organisms/Community Page", //providing path
   component: CommunityPage,
-  argTypes: {},
-  //argTypes: {}, giving user the control based on the Prop-types
-  // i.g.
-  // argTypes: {
-  //    size: {control: 'select'}
-  // }
 } as ComponentMeta<typeof CommunityPage>;
 
 const Template: ComponentStory<typeof CommunityPage> = (
@@ -26,22 +20,21 @@ Primary.args = {
       "AWESOME hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
     likes: 20,
     comments: 50,
-  },
-  userAccountProps: {
-    firstName: "Avantika",
-    lastName: "Mittapally",
-    role: "Advisor",
-    image:
-      "https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-4-470x470.png",
-    title: "Chair of Department",
-    office: "ESB 2101",
-    department: "Engineering Management & Systems Engineering",
+    user: {
+      firstName: 'Dr. Andres',
+      lastName: 'Sousa-Poza',
+      role: 'Prof',
+      image: 'https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-4-470x470.png',
+      title: 'Chair of Department',
+      office: 'ESB 2101',
+      department: 'Engineering Management & Systems Engineering',
+    },
   },
   inputProps: {
     defaultValue: "",
     label: "Search for anything",
     name: "floating_search",
-    onChange: onchange,
+    onChange: (e) => e,
     role: "search",
     type: "search",
   },
@@ -56,22 +49,21 @@ Secondary.args = {
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae error iure officiis exercitationem, commodi ab reiciendis eum ex veritatis placeat amet architecto itaque cumque blanditiis numquam repellat, necessitatibus natus nihil! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae error iure officiis exercitationem, commodi ab reiciendis eum ex veritatis placeat amet architecto itaque cumque blanditiis numquam repellat, necessitatibus natus nihil!",
     likes: 21,
     comments: 55,
-  },
-  userAccountProps: {
-    firstName: "Avantika",
-    lastName: "Mittapally",
-    role: "TA",
-    image:
-      "https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-4-470x470.png",
-    title: "Chair of Department",
-    office: "ESB 2101",
-    department: "Engineering Management & Systems Engineering",
+    user: {
+      firstName: 'Avantika',
+      lastName: 'Mittapally',
+      role: 'Advisor',
+      image: 'https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-4-470x470.png',
+      title: 'Chair of Department',
+      office: 'ESB 2101',
+      department: 'Engineering Management & Systems Engineering',
+    },
   },
   inputProps: {
     defaultValue: "",
     label: "Search for anything",
     name: "floating_search",
-    onChange: onchange,
+    onChange: (e) => e,
     role: "search",
     type: "search",
   },
