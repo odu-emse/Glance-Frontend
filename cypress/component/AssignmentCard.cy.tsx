08bae9ae833d17f7561a64c0c400490b70d64199
Dániel B. Papp <dpapp@odu.edu>
2022-11-07 11:41:29 -0500

    :recycle: (PR): Modified the name of test cases to match standards Remember that all test cases **must** start with the "should" keyword and proceeded by the test case statement.

--- cypress/component/AssignmentCard.cy.tsx
+++ cypress/component/AssignmentCard.cy.tsx
@@ -7,8 +7,8 @@ const { Primary, Secondary, Tertiary } = composeStories(stories)
 
 describe('AssignmentCard.tsx', function () {
 	const questionCount = 68
-    	const moduleIdentifier = 'M 137G'
-    	const moduleName = 'Introduction to Organizational Analysis'
+	const moduleIdentifier = 'M 137G'
+	const moduleName = 'Introduction to Organizational Analysis'
 	const assignmentName = 'Assignment 4'
 	beforeEach(() => {
 		cy.injectAxe()
@@ -27,13 +27,13 @@ describe('AssignmentCard.tsx', function () {
 			}
 		}, terminalLog)
 	})
-	it('questionCount should render following class', function () {
+	it('should render component questionCount with sm text and light color', () => {
 		cy.mount(<Secondary questionCount={questionCount} />)
 		cy.get('p').should('have.css', 'margin', '12px')
 		cy.get('p').should('have.css', 'font-size', '12px')
 		cy.get('p').should('have.class', 'mx-3 my-3 text-gray500 inline-block text-xs')
 	})
-	it('moduleIdentifier should render following class', function () {
+	it('should render component moduleIdentifier with uppercase, sm text and light color', () => {
 		cy.mount(<Primary moduleIdentifier={moduleIdentifier} />)
 		cy.get('p.uppercase').should('have.css', 'margin', '12px')
 		cy.get('p.uppercase').should('have.css', 'font-size', '12px')
@@ -41,7 +41,7 @@ describe('AssignmentCard.tsx', function () {
 		cy.get('p.uppercase').should('have.css', 'text-transform', 'uppercase')
 		cy.get('p.uppercase').should('have.class', 'mx-3 my-3 pl-6 text-gray-500 inline-block text-xs uppercase font-semibold')
 	})
-	it('moduleName, assignmentName should render following class', function () {
+	it('should render component moduleName and assignmentName with lg text and minimal margin', () => {
 		cy.mount(<Tertiary moduleName={moduleName} assignmentName={assignmentName} />)
 		cy.get('p.text-xl').should('have.css', 'margin', '12px')
 		cy.get('p.text-xl').should('have.css', 'font-size', '20px')
