17e43bedcea737c187ec020041ca1d09e441ee25
Dániel B. Papp <dpapp@odu.edu>
2022-11-07 10:58:08 -0500

    :recycle: (PR): Made the textarea component more dynamic Extracted most of the attributes that the element accepts into component props with default values.

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,63 +1,87 @@
-
 import * as React from 'react'
+import { useRef } from 'react'
+import useAutosizeTextArea from './useAutosizeTextArea'
+import { IoSend } from 'react-icons/io5'
 
-import { useRef, useState } from "react";
-
-import useAutosizeTextArea from "./useAutosizeTextArea";
-
-import { IoSend } from "react-icons/io5";
 export const TextArea = ({
-	
+	handle = () => null,
+	value = '',
+	id = 'text-area',
+	role = 'textbox',
+	name = 'text-area',
+	rows = 1,
+	placeholder = '',
+	disabled = false,
+	maxLength = 1000,
+	required = false,
+	readOnly = false,
+	wrap = 'soft',
+	autofocus = false,
+	label = '',
 }: TextAreaProps) => {
-    const [value, setValue] = useState("");
-    const textAreaRef = useRef<HTMLTextAreaElement>(null);
-  
-    useAutosizeTextArea(textAreaRef.current, value);
-  
-    const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
-      const val = evt.target?.value;
-  
-      setValue(val);
-    };
-  
-	return (
-        <div >
-        <label htmlFor="review-text" className="relative" >
-        
-      <textarea
-        id="review-text"
-        onChange={handleChange}
-        ref={textAreaRef}
-        rows={1}
-        value={value}
-        className="w-full  bg-white placeholder:font-italitc border border-slate-400 drop-shadow-md rounded-md py-2 pl-3 pr-10 focus:outline-none"
-      />
-           <span className="absolute onset-y-0 right-0 bottom-4 flex items-center pr-3">
+	const textAreaRef = useRef<HTMLTextAreaElement>(null)
 
-     <button className="">
-								<IoSend
-									size={20}
-									className="mr-1"
-									
-								/>
-							</button>
-            </span>
-      </label>
-    </div>
+	useAutosizeTextArea(textAreaRef.current, value)
+
+	return (
+		<div>
+			<label htmlFor={id} className="">
+				{label && label}
+				<div className="relative">
+					<textarea
+						id={id}
+						onChange={handle}
+						ref={textAreaRef}
+						rows={rows}
+						name={name}
+						role={role}
+						value={value}
+						placeholder={placeholder}
+						disabled={disabled}
+						maxLength={maxLength}
+						required={required}
+						readOnly={readOnly}
+						wrap={wrap}
+						autoFocus={autofocus}
+						className={`w-full bg-white placeholder:italic border border-slate-400 shadow-md rounded-md py-2 pl-3 pr-10 focus:outline-2 focus:outline-dashed focus:ring-0 disabled:opacity-50 disabled:cursor-not-allowed ${
+							value.length === maxLength
+								? 'border-red-400 focus:border-red-500 focus:outline-red-400 focus:ring-red-400'
+								: ' focus:outline-blue-400'
+						}`}
+					/>
+					<span
+						className={`absolute right-2 bottom-2 flex items-center ${
+							disabled ? 'opacity-50' : ''
+						}`}
+					>
+						<button type="button">
+							<IoSend size={20} className="mr-1" />
+						</button>
+					</span>
+				</div>
+				{value.length === maxLength && (
+					<span className="text-xs px-2 bg-gray-100 ml-auto mr-0 block w-fit">
+						Maximum characters: {maxLength}
+					</span>
+				)}
+			</label>
+		</div>
 	)
 }
 
-
 type TextAreaProps = {
-	
-	
+	handle?: (evt: React.ChangeEvent<HTMLTextAreaElement>) => void
+	value: string
+	id?: string
+	role?: string
+	name?: string
+	rows?: number
+	placeholder?: string
+	disabled?: boolean
+	maxLength?: number
+	required?: boolean
+	readOnly?: boolean
+	wrap?: 'soft' | 'hard'
+	autofocus?: boolean
+	label?: string
 }
-
-
-
-
-  
-
-
-
-
--- src/stories/TextArea.stories.tsx
+++ src/stories/TextArea.stories.tsx
@@ -1,19 +1,69 @@
 import * as React from 'react'
 import { TextArea } from '../components/TextArea'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { useState } from 'react'
 
 export default {
-	title: 'Molecules/Text Area',
+	title: 'Atoms/Text Area',
 	component: TextArea,
+	argTypes: {
+		handle: { action: 'handle' },
+		value: { control: 'text' },
+		id: { control: 'text' },
+		role: { control: 'text' },
+		name: { control: 'text' },
+		rows: { control: 'range' },
+		placeholder: { control: 'text' },
+		disabled: { control: 'boolean' },
+		maxLength: { control: 'range' },
+		required: { control: 'boolean' },
+		readOnly: { control: 'boolean' },
+		wrap: { control: 'text' },
+		autofocus: { control: 'boolean' },
+	},
 } as ComponentMeta<typeof TextArea>
 
-const Template: ComponentStory<typeof TextArea> = (args) => (
-	<TextArea {...args} />
-)
-
-export const Primary = Template.bind({})
-Primary.args = {
-	
+const Template: ComponentStory<typeof TextArea> = (args) => {
+	const [value, setValue] = useState('')
+	const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setValue(evt.target?.value)
+	}
+	return <TextArea {...args} value={value} handle={handleChange} />
 }
 
+export const Primary = Template.bind({})
+Primary.storyName = 'Default'
+Primary.args = {
+	value: 'This is a default value text for the text area component.',
+	id: 'text-area',
+	role: 'textbox',
+	name: 'text-area',
+	rows: 1,
+	placeholder: 'This is a placeholder text for the text area component.',
+	disabled: false,
+	maxLength: 1000,
+	required: false,
+	readOnly: false,
+	wrap: 'soft',
+	autofocus: false,
+}
 
+export const Disabled = Template.bind({})
+Disabled.args = {
+	...Primary.args,
+	disabled: true,
+}
+
+export const WithLabel = Template.bind({})
+WithLabel.args = {
+	...Primary.args,
+	label: 'Basic Text Area',
+}
+
+export const MaxLength = Template.bind({})
+MaxLength.storyName = 'Specific Max Length'
+MaxLength.args = {
+	...Primary.args,
+	maxLength: 10,
+	value: 'This is a default value text for the text area component.',
+}
