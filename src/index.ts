8cdf868bd6b44e0a766fd2d0491465cb1acdff7d
Dániel B. Papp <dpapp@odu.edu>
2022-11-10 12:04:46 -0500

    :recycle: (PR): Added more dynamic flexibility to the component Now the select component can be customized extensively thanks to the list of props that are available. The component now also accepts an array of strings and an array of objects for options. Using the object configuration will give the consumer more control over each option's value, state, and label.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './output.css'
 export * from './components/Button'
 export * from './components/Layout'
+export * from './components/Select'
 export * from './components/Sidebar'
 export * from './components/Link'
 export * from './components/Content'
