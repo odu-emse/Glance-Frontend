f55b3f18634468352c202ba00cf52fd8cd063491
Joel DeSante <joeldesante@gmail.com>
2022-11-15 14:03:07 -0500

    Add basic pages for the communities

---
+++ pages/communities/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <p>Communities Index</p>
+    </div>
+  )
+}
+
+export default Index;
---
+++ pages/communities/modules/[module]/index.tsx
@@ -0,0 +1,9 @@
+const Module = () => {
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <p>Communities Module</p>
+        </div>
+    )
+}
+  
+export default Module;
