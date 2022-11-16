2ab091d02d7633cfd60d66bcc831ccc6f2ad683f
Joel DeSante <joeldesante@gmail.com>
2022-11-16 12:40:59 -0500

    Add changes

--- pages/communities/index.tsx
+++ pages/communities/index.tsx
@@ -1,7 +1,10 @@
+import { Thread } from "emse-ui"
+
 const Index = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
       <p>Communities Index</p>
+      <Thread />
     </div>
   )
 }
