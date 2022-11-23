5c583d266f0e6b7997ba0e881c5b1e86f828a904
Vineeth <vpott001@odu.edu>
2022-11-23 00:14:12 -0500

    functionality of notifying the user about copied url

--- src/components/Thread/Thread.tsx
+++ src/components/Thread/Thread.tsx
@@ -30,14 +30,21 @@ export const Thread: React.FC<ThreadProps> = ({
 			<p className="text-sm">{body}</p>
 			<div className="flex flex-row justify-end gap-4">
 				<button
-				 className="text-sm rounded-full px-4 py-2 bg-gray-100 hover:bg-gray-200"
+				 className="group relative text-sm rounded-full px-4 py-2 bg-gray-100 hover:bg-gray-200"
+				 data-bs-toggle="popover" data-bs-placement="top" data-bs-content="copied"
 				 onClick = {() => {
 					url = window.location.href;
 					navigator.clipboard.writeText(url)
+					setIsClicked(true)
 				 } 
 				}
 				 >
 					<TbShare size={18} />
+					{isClicked === true && (
+								<div className="absolute hidden group-hover:flex -left-2 -top-2 -translate-y-full w-16 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+									copied!
+								</div>
+						)}
 				</button>
 				<button className="text-sm rounded-full px-4 py-2 bg-gray-100 hover:bg-gray-200">
 					<GoCommentDiscussion size={18} />
