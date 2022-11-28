42d7b11fbd5fac9911facb7afe7bb871f7d4394b
Joel DeSante <joeldesante@gmail.com>
2022-11-28 14:46:37 -0500

    Change button shape for PostButton component to Pill

--- src/components/Thread/PostButton.tsx
+++ src/components/Thread/PostButton.tsx
@@ -5,12 +5,13 @@ export const PostButton = () => {
 	return (
 		<div className="relative">
 			<textarea
-				className="w-full bg-white placeholder:italic border-4 border-blue-500  rounded-md py-2 pl-3 pr-10 focus:outline-2 focus:outline-dashed focus:ring-0"
-				placeholder="Type Here.."
+				className="w-full bg-white placeholder:italic border-2 border-blue-700 rounded-md py-2 pl-3 pr-10 focus:outline-2 focus:outline-dashed focus:ring-0"
+				placeholder="Leave a comment..."
+				rows={4}
 			></textarea>
 
 			<span className="absolute right-2 bottom-2 flex items-center m-2 ">
-				<Button label="Post" size="small" />
+				<Button size="small" shape='pill'>Post</Button>
 			</span>
 		</div>
 	)
