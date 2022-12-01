650451ec67e8c504bbaf600a2197df9ede2ab744
Divya <drama001@odu.edu>
2022-12-01 11:33:24 -0500

    added minute changes - font size, imports, html entities

--- pages/communities/modules/[module]/index.tsx
+++ pages/communities/modules/[module]/index.tsx
@@ -1,5 +1,4 @@
-import {Thread} from 'emse-ui'
-import {Input} from 'emse-ui'
+import {Thread, Input} from 'emse-ui'
 
 const Module = () => {
     const userName="joeldesante"
@@ -8,21 +7,18 @@ const Module = () => {
     return (
         <div className='m-8 flex'>
             <div className='m-10 grow'>
-                <p className="text-2xl font-bold">{moduleName}</p>
-                <div className="flex my-2">
-                <img
-					src="https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg"
-					alt="profile image"
-					className="shadow-lg rounded-full max-w-full h-4 align-middle border-none"
-				/>
-                    <small className="px-2">{userName}</small>
-                     <small className="px-3">
-                        <ul className="list-disc">
-                            <li>{userEmail}</li> 
-                        </ul></small>
+                <p className="text-3xl font-semibold">{moduleName}</p>
+                <div className="flex my-2 items-center">
+                    <img
+                        src="https://i.pinimg.com/custom_covers/222x/85498161615209203_1636332751.jpg"
+                        alt="profile image"
+                        className="shadow-lg rounded-full max-w-full h-4 align-middle border-none"
+                    />
+                    <small className="pl-2 font-bold">{userName}</small>
+                    <small ><span className='px-1 font-bold'>&bull;</span>{userEmail}</small>
                 </div>
                 <div className="flex items-center">
-                    <h1 className="text-md font-bold flex-none pr-20">Recent Threads</h1>
+                    <h1 className="text-lg font-semibold flex-none pr-20">Recent Threads</h1>
                     <Input 
                         className="grow"
                         defaultValue=""
