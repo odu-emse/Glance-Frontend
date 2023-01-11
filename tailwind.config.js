039360795701af52cd3c314e7bb7802bcf27456a
Yao Wang <ywang011@odu.edu>
2023-01-11 12:37:28 -0500

    fixed issue with storybook css inconsistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
-
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx,mdx}", './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    screens: {
-      'xxs': '320px',
-      'xs': '414px',
-      ...defaultTheme.screens
-    },
-    fontSize: {
-      'xxs': '0.5rem',
-      'xs': '0.75rem',
-      ...defaultTheme.fontSize
-    }
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+	content: [
+		'./src/**/*.{html,js,jsx,ts,tsx,mdx}',
+		'./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+		'./storybook/**/*.{ts,tsx}',
+	],
+	theme: {
+		screens: {
+			xxs: '320px',
+			xs: '414px',
+			...defaultTheme.screens,
+		},
+		fontSize: {
+			xxs: '0.5rem',
+			xs: '0.75rem',
+			...defaultTheme.fontSize,
+		},
+	},
+	plugins: [require('flowbite/plugin')],
 }
