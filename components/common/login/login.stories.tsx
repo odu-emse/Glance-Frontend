ce4b2aba4e130769a50913825b3247757b8770d0
Vineeth <vpott001@odu.edu>
2023-02-02 12:33:06 -0500

    Fixed pre-commit autoformating errors

--- components/common/login/login.stories.tsx
+++ components/common/login/login.stories.tsx
@@ -1,23 +1,15 @@
 import * as React from 'react'
-import { Login }from './login'
+import { Login } from './login'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 export default {
 	title: 'Common/Login',
 	component: Login,
-	argTypes: {
-	
-	},
+	argTypes: {},
 } as ComponentMeta<typeof Login>
 
-const Template: ComponentStory<typeof Login> = (args: any) => (
-	<Login />
-)
+const Template: ComponentStory<typeof Login> = (args: any) => <Login />
 
 export const Primary: ComponentStory<typeof Login> = Template.bind({})
 Primary.storyName = 'Default'
-Primary.args = {
-
-}
-
-
+Primary.args = {}
--- components/common/login/login.tsx
+++ components/common/login/login.tsx
@@ -3,20 +3,19 @@ import { Button } from '../button/button'
 import { useGoogleLogin } from '@react-oauth/google'
 
 export const Login = () => {
+	const login = () => {
+		const login = useGoogleLogin({
+			onSuccess: async (codeResponse) => console.log(codeResponse),
+			onError: (error) => console.log(error),
+			flow: 'auth-code',
+			hosted_domain: 'odu.edu',
+			ux_mode: 'redirect',
+			redirect_uri: 'http://localhost:3000/auth/redirect',
+		})
+	}
 
-    const login = () => {
-        const login = useGoogleLogin({
-            onSuccess: async (codeResponse) => console.log(codeResponse),
-            onError: (error) => console.log(error),
-            flow: 'auth-code',
-            hosted_domain: 'odu.edu',
-            ux_mode: 'redirect',
-            redirect_uri: 'http://localhost:3000/auth/redirect',
-        })
-    }
-
-    return (
-        <div className="h-screen flex items-center">
+	return (
+		<div className="h-screen flex items-center">
 			<div className="grid place-items-center h-1/4 w-full ">
 				<div>
 					<p className="text-7xl font-extrabold font-weight:2000 text-center text-black-3000">
@@ -27,9 +26,7 @@ export const Login = () => {
 					</p>
 				</div>
 
-				<Button onClick={() => login()} >
-                Login with Google
-                </Button>
+				<Button onClick={() => login()}>Login with Google</Button>
 			</div>
 		</div>
 	)
