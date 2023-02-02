f3d0e92ed9ce254fb78520caa4c668cb17c66585
Vineeth <vpott001@odu.edu>
2023-02-02 12:26:41 -0500

    Login Page converted to a StoryBook Component

---
+++ components/common/login/login.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Button } from '../button/button'
+import { useGoogleLogin } from '@react-oauth/google'
+
+export const Login = () => {
+
+    const login = () => {
+        const login = useGoogleLogin({
+            onSuccess: async (codeResponse) => console.log(codeResponse),
+            onError: (error) => console.log(error),
+            flow: 'auth-code',
+            hosted_domain: 'odu.edu',
+            ux_mode: 'redirect',
+            redirect_uri: 'http://localhost:3000/auth/redirect',
+        })
+    }
+
+    return (
+        <div className="h-screen flex items-center">
+			<div className="grid place-items-center h-1/4 w-full ">
+				<div>
+					<p className="text-7xl font-extrabold font-weight:2000 text-center text-black-3000">
+						ALMP
+					</p>
+					<p className="text-black-400 font-semibold text-xl">
+						You must be logged in to access this resource.
+					</p>
+				</div>
+
+				<Button onClick={() => login()} >
+                Login with Google
+                </Button>
+			</div>
+		</div>
+	)
+}
