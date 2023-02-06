c4478b66c5b30b1e39ba63e87d58c80087ce3e45
Yao Wang <ywang011@odu.edu>
2023-02-06 13:26:35 -0500

    created placeholder for toggleOption

---
+++ components/common/toggle/toggle.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+export const ToggleOption: React.FC<ToggleProps> = ({
+
+}): React.ReactElement => {
+    return(
+        <div></div>
+    )
+}
+
+
+export type ToggleProps = {
+    toggleStatusIdentifier: number
+}
+export type AssignmentPropType = {
+	type: string
+	average: number
+}
