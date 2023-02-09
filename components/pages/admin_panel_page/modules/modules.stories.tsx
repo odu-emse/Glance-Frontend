09c166aec7a1fc4b3af579615c9e5553b0dbeb3d
Divya <drama001@odu.edu>
2023-02-09 12:28:39 -0500

    added draggable and droppable functionality

--- components/pages/admin_panel_page/modules/modules.stories.tsx
+++ components/pages/admin_panel_page/modules/modules.stories.tsx
@@ -2,16 +2,13 @@ import * as React from 'react'
 import { ModuleProps, Modules } from './modules'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
-
 export default {
 	title: 'Pages/AdminPanel/Modules',
 	component: Modules,
 	argTypes: {},
 } as ComponentMeta<typeof Modules>
 
-const Template: ComponentStory<typeof Modules> = (args) => (
-	<Modules {...args} />
-)
+const Template: ComponentStory<typeof Modules> = (args) => <Modules {...args} />
 
 export const Default: ComponentStory<typeof Modules> = Template.bind({})
 Default.args = {}
--- components/pages/admin_panel_page/modules/modules.tsx
+++ components/pages/admin_panel_page/modules/modules.tsx
@@ -1,33 +1,51 @@
-import { ModuleCard, ModuleCardProps } from "../../../common/pages/admin_panel/module_card/module_card"
+import {
+	ModuleCard,
+	ModuleCardProps,
+} from '../../../common/pages/admin_panel/module_card/module_card'
+import { DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 
-export const Modules = ({
-    moduleCardProps,
-    modules = []
-}:ModuleProps) => {
+export const Modules = ({ 
+    moduleCardProps, 
+    modules = [] 
+}: ModuleProps) => {
+    
+	for (let i = 0; i < 4; i++) {
+		modules.push(
+            <DragDropContext>
+			<div className="m-3">
+                <Droppable droppableId="module">
+                    {(provided:any) => (
+                        <ul className="modules" {...provided.droppableProps} ref={provided.innerRef}>
+                            <Draggable key={i} draggableId="j" index={i}>
+                                {(provided:any)=>(
+                                    <li key={i} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >
+                                        <ModuleCard 
+                                            moduleName={'Module A'}
+                                            instructorName={'Instructor'}
+                                            registeredUsers={34}
+                                        />
+                                    </li>
+                                )}
+                            </Draggable>
+                            {provided.placeholder}
+                        </ul>
+                        )
+                    }
+                </Droppable>
+			</div>
+            </DragDropContext>
+		)
+	}
 
-    for(let i=0; i<4; i++){
-        modules.push(
-            <div className="m-3">
-                <ModuleCard key={i}
-                    moduleName={"Module A"} 
-                    instructorName={"Instructor"} 
-                    registeredUsers={34}              
-                />
-            </div>
-        );
-    }
-
-    return(
-        <>
-        <h1 className="text-xl m-3 py-2">Your Modules</h1>
-        <div className="moduleCard">
-        {modules}
-        </div>
-        </>
-    )
+	return (
+		<>
+			<h1 className="text-xl m-3 py-2">Your Modules</h1>
+			<div className="moduleCard">{modules}</div>
+		</>
+	)
 }
 
 export type ModuleProps = {
-    moduleCardProps: ModuleCardProps,
-    modules: any,
+	moduleCardProps: ModuleCardProps,
+	modules: Array<any>
 }
