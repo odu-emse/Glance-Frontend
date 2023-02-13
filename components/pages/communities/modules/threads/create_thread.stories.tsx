084a83f3d5dcbafca7cce98128f6d48e95896ff2
Divya <drama001@odu.edu>
2023-02-13 14:35:31 -0500

    added thread page for the user to add threads in module

---
+++ components/pages/communities/modules/threads/create_thread.stories.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { ModuleThread } from './create_thread'
+
+export default {
+	title: 'Pages/Communities/Modules/Threads',
+	component: ModuleThread,
+	argTypes: {},
+} as ComponentMeta<typeof ModuleThread>
+
+const Template: ComponentStory<typeof ModuleThread> = (
+	args:any
+) => {
+	return <ModuleThread {...args} />
+	
+}
+
+export const Primary: ComponentStory<typeof ModuleThread> = Template.bind({})
+Primary.storyName = 'Default'
+Primary.args = {}
