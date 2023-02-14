a2ceb4a07973f833e0a10b479fad7bf09d67678d
Vineeth <vpott001@odu.edu>
2023-02-14 13:09:40 -0500

    Lesson Component Area Created

--- components/common/admin_panel/content_area.tsx
+++ components/common/admin_panel/content_area.tsx
@@ -1,32 +1,58 @@
-import React, {useCallback} from 'react';
-import {useDropzone} from 'react-dropzone';
+import React from 'react'
+import { useDropzone } from 'react-dropzone'
 
 export const ContentArea = (): React.ReactElement => {
-    const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
-  
-    const files = acceptedFiles.map(file => (
-      <li key={file.path}>
-        {file.path} - {file.size} bytes
-      </li>
-    ));
-    return (
-      <section className="container">
-        <div {...getRootProps({className: 'dropzone'})}>
-          <input {...getInputProps()} />
-            <div className="flex items-center justify-center w-auto">
-                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>  
-                </div>
-                    <input id="dropzone-file" type="file" className="hidden" />
-                </label>
-            </div> 
-        </div>
-        <aside>
-          <h4>Uploaded Files:</h4>
-          <ul>{files}</ul>
-        </aside>
-      </section>
-    );
+	const { acceptedFiles, getRootProps, getInputProps } = useDropzone()
+
+	const files = acceptedFiles.map((file) => (
+		<li key={file.path}>
+			{file.path} - {file.size} bytes
+		</li>
+	))
+	return (
+		<section className="container">
+			<div {...getRootProps({ className: 'dropzone' })}>
+				<input {...getInputProps()} />
+				<div className="flex items-center justify-center w-auto">
+					<label
+						for="dropzone-file"
+						class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+					>
+						<div className="flex flex-col items-center justify-center pt-5 pb-6">
+							<svg
+								aria-hidden="true"
+								className="w-10 h-10 mb-3 text-gray-400"
+								fill="none"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+								></path>
+							</svg>
+							<p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+								<span className="font-semibold">
+									Click to upload
+								</span>{' '}
+								or drag and drop
+							</p>
+						</div>
+						<input
+							id="dropzone-file"
+							type="file"
+							className="hidden"
+						/>
+					</label>
+				</div>
+			</div>
+			<aside>
+				<h4>Uploaded Files:</h4>
+				<ul>{files}</ul>
+			</aside>
+		</section>
+	)
 }
