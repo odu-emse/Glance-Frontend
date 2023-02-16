6e19f693bd42a04fee063a8689da70dd51225477
Vineeth <vpott001@odu.edu>
2023-02-16 11:42:13 -0500

    Documented the Content Area Component

--- components/common/admin_panel/content_area.tsx
+++ components/common/admin_panel/content_area.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDropzone } from 'react-dropzone'
 
-export const ContentArea = (): React.ReactElement => {
+export const ContentArea: React.FC<ContentArea> = ( { }): React.ReactElement => {
 	const { acceptedFiles, getRootProps, getInputProps } = useDropzone()
 
 	const files = acceptedFiles.map((file) => (
@@ -21,7 +21,7 @@ export const ContentArea = (): React.ReactElement => {
 						<div className="flex flex-col items-center justify-center pt-5 pb-6">
 							<svg
 								aria-hidden="true"
-								className="w-10 h-10 mb-3 text-gray-400"
+								className="w-full h-full mb-3 text-gray-400"
 								fill="none"
 								stroke="currentColor"
 								viewBox="0 0 24 24"
@@ -34,7 +34,7 @@ export const ContentArea = (): React.ReactElement => {
 									d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
 								></path>
 							</svg>
-							<p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+							<p className="mb-2 text-xl text-gray-500 dark:text-gray-400">
 								<span className="font-semibold">
 									Click to upload
 								</span>{' '}
@@ -55,4 +55,23 @@ export const ContentArea = (): React.ReactElement => {
 			</aside>
 		</section>
 	)
+}
+
+export type ContentArea = {
+    /**
+    * useDropzone hook just binds the necessary handlers to create a drag 'n' drop zone
+    */
+    useDropzone: string
+    /**
+    * the getRootProps() fn to get the props required for drag 'n' drop and use them on any element.
+    */
+    getRootProps: Function
+    /**
+    *  the getInputProps() fn is used for click and keydown behavior
+    */
+    getInputProps: Function
+    /**
+    *  the acceptedFiles() fn is used for track and map the uploaded files to the list
+    */
+    acceptedFiles: Function
 }
