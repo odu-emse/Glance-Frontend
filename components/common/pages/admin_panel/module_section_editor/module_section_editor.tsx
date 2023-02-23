5cc8f8358e9bf0a5246ff0e55c817dfa33cf346c
Divya <drama001@odu.edu>
2023-02-23 15:04:53 -0500

    added functionality expand or collapse

---
+++ components/common/pages/admin_panel/module_section_editor/module_section_editor.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+export const ModuleSectionEditor = () => {
+    const [isclicked, setIsClicked] = useState(false);
+    const showContent = () => setIsClicked(!isclicked);
+    return(
+        <>
+            <button className="sectionToggle border-solid border-gray bg-gray-200  hover:bg-gray-100 rounded border text-2xl "
+                    onClick={showContent}>
+                <span className="m-2">Section Component</span>
+			</button>	
+                <div className="sectionLessons">
+                    {isclicked ? <div>Lesson Component</div>: ""}
+                </div>	
+
+        </>
+    )
+}
