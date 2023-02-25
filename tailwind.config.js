d0285fb28e183d44cd421d2beaf81fd2a060393d
Joel DeSante <joeldesante@gmail.com>
2023-02-24 20:57:49 -0500

    Implement new standards rules.

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,15 @@ module.exports = {
 			xs: '0.75rem',
 			...defaultTheme.fontSize,
 		},
+		colors: {
+			black: '#0A192D' /* Midnight Blue */,
+			white: '#FFFBFB' /* Vanilla Icecream White (Lightly warmed for ease on eyes) */,
+			carrier: '#404A5A' /* Carrier Gray */,
+			royalblue: '#204498' /* Royal Blue */,
+			monarchblue: '#003057' /* Monarch Blue */,
+			wgray: '#D6D5DA' /* USS Wisconsin Grey */,
+			barryblue: '#7B9CCF' /* Barry Blue */,
+		},
 	},
 	plugins: [require('flowbite/plugin')],
 }
