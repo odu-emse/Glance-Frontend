d26587cbc01ac0fd3edb583149c752321768c1af
Divya <drama001@odu.edu>
2023-02-27 11:40:12 -0500

    prettier changes

--- components/common/pages/admin_panel/module_section_editor/module_section_editor.tsx
+++ components/common/pages/admin_panel/module_section_editor/module_section_editor.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react"
+import { useState } from 'react'
 export const ModuleSectionEditor = () => {
-    const [isclicked, setIsClicked] = useState(false);
-    const showContent = () => setIsClicked(!isclicked);
-    return(
-        <>
-            <button className="sectionToggle border-solid border-gray bg-gray-200  hover:bg-gray-100 rounded border text-2xl "
-                    onClick={showContent}>
-                <span className="m-2">Section Component</span>
-			</button>	
-                <div className="sectionLessons">
-                    {isclicked ? <div>Lesson Component</div>: ""}
-                </div>	
-
-        </>
-    )
+	const [isclicked, setIsClicked] = useState(false)
+	const showContent = () => setIsClicked(!isclicked)
+	return (
+		<>
+			<button
+				className="sectionToggle border-solid border-gray bg-gray-200  hover:bg-gray-100 rounded border text-2xl "
+				onClick={showContent}
+			>
+				<span className="m-2">Section Component</span>
+			</button>
+			<div className="sectionLessons">
+				{isclicked ? <div>Lesson Component</div> : ''}
+			</div>
+		</>
+	)
 }
