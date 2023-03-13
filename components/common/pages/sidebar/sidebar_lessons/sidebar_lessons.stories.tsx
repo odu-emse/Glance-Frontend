55e6be55b2f14052875e0d172fa15d8f5cc019a8
Vineeth <vpott001@odu.edu>
2023-03-13 17:49:53 -0400

    ALMP-598 Sidebar re-implemented as per the new style guide

--- components/common/pages/sidebar/sidebar_lessons/sidebar_lessons.stories.tsx
+++ components/common/pages/sidebar/sidebar_lessons/sidebar_lessons.stories.tsx
@@ -2,21 +2,15 @@ import * as React from 'react'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { SidebarLessons, SidebarLessonsProps } from './sidebar_lessons'
 
-	
-	export default {
-		title: 'Common/Sidebar/Sidebar',
-		component: SidebarLessons,
-		argTypes: {},
-	} as ComponentMeta<typeof SidebarLessons>
-	
-   const Template: ComponentStory<typeof SidebarLessons> = (args: SidebarLessonsProps) => (
-		<SidebarLessons {...args} />
-	) 
+export default {
+	title: 'Common/Sidebar/Sidebar',
+	component: SidebarLessons,
+	argTypes: {},
+} as ComponentMeta<typeof SidebarLessons>
 
-	
-	export const Default = Template.bind({})
-	Default.args = {
-		
-	}
-	
+const Template: ComponentStory<typeof SidebarLessons> = (
+	args: SidebarLessonsProps
+) => <SidebarLessons {...args} />
 
+export const Default = Template.bind({})
+Default.args = {}
--- components/common/pages/sidebar/sidebar_lessons/sidebar_lessons.tsx
+++ components/common/pages/sidebar/sidebar_lessons/sidebar_lessons.tsx
@@ -1,40 +1,41 @@
-import { Anchor } from 'components/common/links/anchor/anchor';
+import { Anchor } from 'components/common/links/anchor/anchor'
 import * as React from 'react'
 
 export const SidebarLessons = () => {
-
-	const lessons = ["Operations Research", "Supply Chain Management", "Linear Regression", "Nonlinear Algebra", "OR Quiz 1"];
+	const lessons = [
+		'Operations Research',
+		'Supply Chain Management',
+		'Linear Regression',
+		'Nonlinear Algebra',
+		'OR Quiz 1',
+	]
 	return (
 		<div className="overflow-x-hidden max-w-screen relative min-h-screen">
-			
-		<aside
-			id="sidePanel"
-			className={`absolute ${
-				open ? 'right-0' : '-right-full'
-			} top-0 h-screen overflow-y-scroll w-96 bg-white transition-all drop-shadow-lg p-0`}
-		>
-			
-				<div className='flex p-6 mt-6 text-xl'>
-				<h2>LESSONS OVERVIEW</h2>
+			<aside
+				id="sidePanel"
+				className={`absolute ${
+					open ? 'right-0' : '-right-full'
+				} top-0 h-screen overflow-y-scroll w-96 bg-white transition-all drop-shadow-lg p-0`}
+			>
+				<div className="flex p-6 mt-6 text-xl">
+					<h2>LESSONS OVERVIEW</h2>
 				</div>
-			
-				{
-					lessons.map((lesson, index)=>(
-					<div className="font-montserrat text-blue mx-3 blue-800 flex text-base"><Anchor
 
-						key={index}
-						path={`/lesson`}
-						role="lesson link"
-						className="font-montserrat text-blue m-3 blue-800 flex text-base"
-					>
-						{lesson}
-					</Anchor></div>
-					))
-				}	
-			</aside></div>
+				{lessons.map((lesson, index) => (
+					<div className="font-montserrat text-blue mx-3 blue-800 flex text-base">
+						<Anchor
+							key={index}
+							path={`/lesson`}
+							role="lesson link"
+							className="font-montserrat text-blue m-3 blue-800 flex text-base"
+						>
+							{lesson}
+						</Anchor>
+					</div>
+				))}
+			</aside>
+		</div>
 	)
 }
 
 export type SidebarLessonsProps = {}
-
-
