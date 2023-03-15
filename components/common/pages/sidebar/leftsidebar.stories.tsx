59fcaba877adecd2e186452d159c84bbd48a38c8
Vineeth <vpott001@odu.edu>
2023-03-15 14:36:24 -0400

    unwanted code

--- components/common/pages/sidebar/leftsidebar.stories.tsx
+++ components/common/pages/sidebar/leftsidebar.stories.tsx
@@ -9,6 +9,18 @@ export default {
 
 const Template = (args) => <Sidebar {...args} />
 
+const Template: ComponentStory<typeof Sidebar> = (args) => {
+	const [open, setOpen] = React.useState(true)
+	return (
+		<div className="overflow-x-hidden max-w-screen relative min-h-screen">
+			<button onClick={() => setOpen(!open)}>
+				{open ? 'Close panel' : 'Open panel'}
+			</button>
+			<Sidebar {...args} handle={setOpen} open={open} />
+		</div>
+	)
+}
+
 export const Default: ComponentStory<typeof Sidebar> = Template.bind({})
 Default.args = {
 icon: "https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-4-470x470.png"	
