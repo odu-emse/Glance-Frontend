0e2a47d4df679fb72d02fff49330487ac51d79fc
Dániel B. Papp <dpapp@odu.edu>
2023-03-16 10:27:56 -0400

    Refactored pipeline jobs and added main branch jobs

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,11 +4,24 @@ import { Button } from '@/common/button/button'
 const NotFound = () => {
 	return (
 		<div className="mx-auto w-full flex flex-col items-center justify-center h-screen">
-			<h1 className="font-semibold text-7xl">404 - Not Found!</h1>
-			<Link className="underline text-blue-500" href="/">
-				<Button>Return Home</Button>
-			</Link>
-		</div>
+			<h1 			className="font-semibold text-7xl"
+
+
+			>404 - Not Found!
+
+			</h1
+
+			>
+	<Link className="underline text-blue-500" href="/">
+				<Button>
+
+
+								Return Home
+
+
+</Button>
+	</Link>
+							</div>
 	)
 }
 
