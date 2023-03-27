edfb9dd5684942920410fd8fd4177c09b08717ba
Divya <drama001@odu.edu>
2023-03-27 14:54:57 -0400

    added login button to the landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 //import "styles/pages/index.css"
+import { Button } from '@/components/common/button/button'
 import Head from 'next/head'
 
 // background: rgb(32,68,152);
@@ -11,14 +12,24 @@ const Index = () => {
 				<title>ALMP</title>
 			</Head>
 			<div
-				id="landing-box"
+			id="landing-box"
+			style={{
+				backgroundImage:
+					"linear-gradient(0deg, rgba(32,68,152,1) 0%, rgba(32,68,152,0.7) 100%), url('/images/student-gettin-outta-here.jpg')",
+				backgroundSize: 'cover',
+				backgroundPosition: 'center',
+			}}
+			>
+				<a className="w-32" href="/login">
+					<button
+						type="button"
+						className=" absolute top-8 right-20 h-11 w-24 inline-block rounded-md bg-white text-base font-semibold text-royalblue-300 uppercase drop-shadow-lg leading-normal hover:bg-gray-200">
+						Login
+					</button>
+				</a>
+				
+			<div
 				className="w-screen h-screen flex items-center justify-center flex-col"
-				style={{
-					backgroundImage:
-						"linear-gradient(0deg, rgba(32,68,152,1) 0%, rgba(32,68,152,0.7) 100%), url('/images/student-gettin-outta-here.jpg')",
-					backgroundSize: 'cover',
-					backgroundPosition: 'center',
-				}}
 			>
 				<h1
 					style={{
@@ -40,6 +51,7 @@ const Index = () => {
 					<span>&bull;</span>
 					<small className="m-0 p-0">FE Version 0.3.0</small>
 				</span>
+			</div>
 			</div>
 		</>
 	)
