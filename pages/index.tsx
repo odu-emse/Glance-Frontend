8758fd7600b11b694d61c6f0509048a2efd53b76
Dániel B. Papp <chef-danny-d@users.noreply.github.com>
2023-03-29 18:42:37 +0000

    Automated formatting changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,18 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 
-
 // background: rgb(32,68,152);
 // background: linear-gradient(0deg, rgba(32,68,152,1) 0%, rgba(32,68,152,0.7) 100%);
 
 const Index = () => {
 	const { status } = useSession()
-	const { push } = useRouter();
+	const { push } = useRouter()
 
 	console.log(status)
-	if(status === "authenticated") {
+	if (status === 'authenticated') {
 		// Redirect to modules page
-		push("/modules")
+		push('/modules')
 	}
-
 
 	return (
 		<>
@@ -38,28 +36,28 @@ const Index = () => {
 				}}
 			>
 				<div className="flex items-center justify-center">
-				<Image 
-					className=""
-					src="/logos/ALMPLogo_New2_White.svg"
-					alt="glance"
-					width={100}
-					height={90}
+					<Image
+						className=""
+						src="/logos/ALMPLogo_New2_White.svg"
+						alt="glance"
+						width={100}
+						height={90}
 					/>
-				
-				<h1
-					style={{
-						fontWeight: 700,
-						fontSize: '52px',
-					}}
-					className="text-white"
-				>
-					GLANCE
-				</h1>
+
+					<h1
+						style={{
+							fontWeight: 700,
+							fontSize: '52px',
+						}}
+						className="text-white"
+					>
+						GLANCE
+					</h1>
 				</div>
 
-				<h5
-				className=" text-white sans text-sm leading-5 font-bold"
-				>Education At A Glance</h5>
+				<h5 className=" text-white sans text-sm leading-5 font-bold">
+					Education At A Glance
+				</h5>
 
 				<Link href={'/login'} passHref>
 					<Button variant="white" className="m-5">
@@ -67,14 +65,11 @@ const Index = () => {
 					</Button>
 				</Link>
 
-				<span
-					className="sans font-bold absolute bottom-1 left-3 gap-4 sans text-white text-sm"
-				>
+				<span className="sans font-bold absolute bottom-1 left-3 gap-4 sans text-white text-sm">
 					<small className="m-0 p-0">API Version 0.3.5</small>
 					<span className="mx-1">&bull;</span>
 					<small className="m-0 p-0">FE Version 0.3.0</small>
 				</span>
-				
 			</div>
 		</>
 	)
