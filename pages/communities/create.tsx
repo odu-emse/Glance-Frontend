f1f2a79cbac8af2015d15145a387733f93459b0e
Dániel B. Papp <chef-danny-d@users.noreply.github.com>
2023-03-29 16:45:38 +0000

    Automated formatting changes

--- pages/communities/create.tsx
+++ pages/communities/create.tsx
@@ -3,17 +3,27 @@ import { Button } from '@/components/common/button/button'
 const CreateThread = () => {
 	return (
 		<div className="flex flex-col">
-			<input type="text" placeholder='An eye catching title...' />
+			<input type="text" placeholder="An eye catching title..." />
 			<select name="aaa" id="">
 				<option value="a">Hello 0</option>
 				<option value="a">Hello 1</option>
 				<option value="a">Hello 2</option>
 				<option value="a">Hello 3</option>
 			</select>
-			<textarea name="a" id="" cols="30" rows="10" placeholder='What do you have to say for yourself?'></textarea>
-			<Button onClick={() => {
-				console.log("Post")
-			}} >Post</Button>
+			<textarea
+				name="a"
+				id=""
+				cols="30"
+				rows="10"
+				placeholder="What do you have to say for yourself?"
+			></textarea>
+			<Button
+				onClick={() => {
+					console.log('Post')
+				}}
+			>
+				Post
+			</Button>
 		</div>
 	)
 }
