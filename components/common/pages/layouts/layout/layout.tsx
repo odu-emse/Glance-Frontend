4eefff650c2c7eae34f12eb649952bdfe8323f2a
Dániel B. Papp <dpapp@odu.edu>
2023-03-30 11:44:00 -0400

    fix(context): Fixed issues with setting profile on load

--- components/common/pages/layouts/layout/layout.tsx
+++ components/common/pages/layouts/layout/layout.tsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react';
 import GlobalLoadingContext from '@/contexts/global_loading_context'
 import Loader from '@/components/util/loader'
 import { Sidebar } from '../../sidebar/sidebar'
 import { useSession } from 'next-auth/react'
+import GlobalUserContext from '@/contexts/global_user_context';
+import gqlFetcher from '@/utils/gql_fetcher';
+import useSWR from 'swr';
+import { gql } from 'graphql-request';
 
 export const Layout = ({ children }) => {
 	const [isLoading, setLoading] = useState(false)
 	const [open, setOpen] = useState(true)
 	const { data: session, status } = useSession()
+	const {setUser} = useContext(GlobalUserContext)
+
+	const {data, error} = useSWR(session ? {
+		query: gql`
+			query {
+				user(input: {
+					openID: "${session.openId}"
+				}){
+					id
+					plan {
+							id
+					}
+				}
+			}
+		`
+	} : null, gqlFetcher)
 
 	if (status === 'loading') {
 		return (
@@ -17,8 +37,25 @@ export const Layout = ({ children }) => {
 		)
 	}
 
+	if (error) {
+		return (
+			<div className="flex justify-center items-center stdcontainer h-screen">
+				<h1 className="text-2xl text-royalblue">Error loading user data</h1>
+			</div>
+		)
+	}
+
+	if(!data){
+		return (
+			<div className="flex justify-center items-center stdcontainer h-screen">
+				<Loader textColor="royalblue" />
+			</div>
+		)
+	}
+
 	return (
 		<section>
+			<GlobalUserContext.Provider value={{ user: data.user[0] || null, setUser }}>
 			<nav className="bg-royalblue stdcontainer-sharp"></nav>
 			<div className="flex h-full">
 				<Sidebar
@@ -46,6 +83,7 @@ export const Layout = ({ children }) => {
 					</GlobalLoadingContext.Provider>
 				</main>
 			</div>
+			</GlobalUserContext.Provider>
 		</section>
 	)
 }
