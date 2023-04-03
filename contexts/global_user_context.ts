05cbf7875f7b9a2bce26914f9435da4b00b06064
Dániel B. Papp <dpapp@odu.edu>
2023-04-03 12:54:36 -0400

    feat(context): Added type def to global user object

--- contexts/global_user_context.ts
+++ contexts/global_user_context.ts
@@ -3,10 +3,14 @@
  * This context can be used to retrieve the current user without having to pass it down the component tree.
  */
 import { createContext } from 'react'
+import { UserAccount } from '@/common/community/threads/thread/thread';
 
 const GlobalUserContext = createContext({
 	user: null,
 	setUser: (user) => {},
+} as {
+	user: Omit<UserAccount & { openID: string, biography?: string }, 'id'> & {id: string} | null
+	setUser: (user: Omit<UserAccount & { openID: string, biography?: string }, 'id'> & {id: string} | null) => void
 })
 
 export default GlobalUserContext
