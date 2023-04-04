e28623f398c185d01d32a7209436941e34d40f84
Dániel B. Papp <dpapp@odu.edu>
2023-04-04 12:34:04 -0400

    fix(community): Added integration for comment addition on main thread

--- components/common/community/threads/thread_text_area/thread_text_area.tsx
+++ components/common/community/threads/thread_text_area/thread_text_area.tsx
@@ -1,16 +1,19 @@
 import { Button } from '../../../button/button'
+import { TextArea } from '@/common/forms/inputs/text_area/text_area';
+import React from 'react';
 
-export const ThreadTextArea = () => {
+export const ThreadTextArea = ({handle, onSubmit}) => {
 	return (
 		<div>
-			<textarea
+			<TextArea
+				handle={(e) => handle(e.target.value)}
 				className="w-full bg-white placeholder:italic border-2 border-royalblue rounded-sm py-2 pl-3 pr-10 focus:outline-2 focus:outline-dashed focus:ring-0"
 				placeholder="What are your thoughts?"
 				rows={4}
-			></textarea>
-
+				icon={false}
+			/>
 			<span className="flex justify-end items-center m-2 ">
-				<Button size="small" shape="regular">
+				<Button size="small" shape="regular" onClick={onSubmit}>
 					Post
 				</Button>
 			</span>
