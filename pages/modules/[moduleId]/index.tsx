e39e847c116de785d36cd702aa9bf9a97543a7f6
Dániel B. Papp <dpapp@odu.edu>
2023-04-06 15:13:56 -0400

    feat(module): add support for fetching lessons by module enrollment The `getModuleByID` function now includes a query to fetch lessons by module enrollment. This allows the application to display the user's progress in the module. The `Module` component now also includes a new `useSWR` hook to fetch user data and pass it as a variable to the `getModuleByID` query.

--- pages/modules/[moduleId]/index.tsx
+++ pages/modules/[moduleId]/index.tsx
@@ -8,24 +8,48 @@ import { Button } from '@/components/common/button/button'
 import Link from 'next/link'
 import GlobalLoadingContext from '@/contexts/global_loading_context'
 import { useContext } from 'react'
+import GlobalUserContext from '@/contexts/global_user_context';
+import { gql } from 'graphql-request';
+import { User } from '../../../types';
 
 const Module = () => {
 	const { setLoading } = useContext(GlobalLoadingContext)
+	const { data: session, status } = useSession()
 	setLoading(true)
 
 	const router = useRouter()
 	const { moduleId } = router.query
 
-	const { data: session, status } = useSession()
+	const { data: userData, error: userError } = useSWR(
+		session
+			? {
+				query: gql`{
+            user(input:{
+                openID: "${session?.openId}"
+            }){
+                id
+                plan {
+                    id
+                }
+            }
+        }`,
+			}
+			: null,
+		gqlFetcher
+	) as { data: { user: Array<User> }; error: Error }
+
 	const { data, error } = useSWR(
-		status !== 'loading'
-			? { query: getModuleByID(moduleId), token: session.idToken }
+		status !== 'loading' && userData
+			? { query: getModuleByID, token: session.idToken, variables: {
+				moduleID: moduleId as string,
+				planID: userData?.user[0].plan.id
+				} }
 			: null,
 		gqlFetcher
 	)
 
 	if (status === 'loading') return
-	if (error) {
+	if (error || userError) {
 		console.log(error)
 		setLoading(false)
 		return (
--- scripts/get_module_by_id.ts
+++ scripts/get_module_by_id.ts
@@ -1,44 +1,57 @@
 import { gql } from 'graphql-request'
 
-export const getModuleByID = (courseID) => {
-	return gql`{
-		module (
-			input:{
-    			id: "${courseID}"
-  			}
-		) {
-			id,
-			members {
-				role
-				plan {
-					student {
-						firstName
-						lastName
-					}
-				}
-			}
-			moduleName,
-			moduleNumber,
-			description,
-			parentModules {
-				id,
-				moduleName,
-				moduleNumber
-			}
-			objectives,
-			collections{
-				id,
-				name,
-				lessons{
-					id,
-					name,
-					content {
-					id,
-					type,
-					link
-					},
-				}
-			}
-		}
-	}`
-}
+export const getModuleByID = gql`query ModuleHomePage($moduleID:ID!, $planID: ID!){
+      module(input:{
+          id: $moduleID
+      }){
+          moduleNumber
+          moduleName
+          id
+        members {
+          role
+          plan {
+            student {
+              firstName
+              lastName
+            }
+          }
+        }
+        description,
+        parentModules {
+          id,
+          moduleName,
+          moduleNumber
+        }
+        objectives,
+        collections{
+          id,
+          name,
+          lessons{
+            id,
+            name,
+            content {
+              id,
+              type,
+              link
+            },
+          }
+        }
+      }
+      lessonsByModuleEnrollment(
+          planID:$planID
+          moduleID: $moduleID
+      ){
+          id
+          name
+          position
+          lessonProgress{
+              status
+              completed
+              updatedAt
+              enrollment{
+                  id
+              }
+          }
+      }
+  }`
+
