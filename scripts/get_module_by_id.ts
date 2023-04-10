5299a08637fc7d73f38aeb4fe81d9e2a750a25b8
Dániel B. Papp <dpapp@odu.edu>
2023-04-10 15:57:36 -0400

    refactor(get_module_by_id.ts): split getModuleByID into two queries The getModuleByID function has been split into two separate queries, getModuleByIDUnenrolled and getModuleByIDEnrolled. This improves code readability and maintainability by separating concerns and making the code more modular.

--- scripts/get_module_by_id.ts
+++ scripts/get_module_by_id.ts
@@ -1,62 +1,67 @@
 import { gql } from 'graphql-request'
 
-export const getModuleByID = gql`
-	query ModuleHomePage($moduleID: ID!, $planID: ID!) {
-		module(input: { id: $moduleID }) {
-			moduleNumber
-			moduleName
-			id
-			members {
-				role
-				plan {
-					student {
-						firstName
-						lastName
-					}
-				}
-			}
-			description
-			parentModules {
-				id
-				moduleName
-				moduleNumber
-			}
-			objectives
-			collections {
-				id
-				name
-				lessons {
-					id
-					name
-					content {
-						id
-						type
-						link
-					}
-				}
-			}
+export const getModuleByIDUnenrolled = gql`
+    query UnEnrolledModuleHomePage($moduleID: ID!) {
+        module(input: { id: $moduleID }) {
+            moduleNumber
+            moduleName
+            id
+            members {
+                role
+                plan {
+                    student {
+                        firstName
+                        lastName
+                    }
+                }
+            }
+            description
+            parentModules {
+                id
+                moduleName
+                moduleNumber
+            }
+            objectives
+            collections {
+                id
+                name
+                lessons {
+                    id
+                    name
+                    content {
+                        id
+                        type
+                        link
+                    }
+                }
+            }
+        }
+    }
+`
+
+export const getModuleByIDEnrolled = gql`
+    query ModuleHomePageLessonProgress($moduleID: ID!, $planID: ID!) {
+        lessonsByModuleEnrollment(planID: $planID, moduleID: $moduleID) {
+            id
+            name
+            position
+            collection {
+                id
+                name
+                position
+                module {
+                    id
+                    moduleName
+                }
+            }
+            lessonProgress {
+                status
+                completed
+                updatedAt
+                enrollment {
+                    id
+                }
+            }
+        }
 		}
-		lessonsByModuleEnrollment(planID: $planID, moduleID: $moduleID) {
-			id
-			name
-			position
-			collection {
-				id
-				name
-				position
-				module {
-					id
-					moduleName
-				}
-			}
-			lessonProgress {
-				status
-				completed
-				updatedAt
-				enrollment {
-					id
-				}
-			}
-		}
-	}
 `
