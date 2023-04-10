46a16987010e6179c676d0c84c92ea329cf71971
Joel DeSante <joeldesante@gmail.com>
2023-04-10 16:54:15 -0400

    Add AXE testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
+import React from 'react';
 import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
 
+const isServerSideRendered = () => {
+	return typeof window === 'undefined';
+}
+
 function App({ Component, pageProps: { session, ...pageProps } }) {
+
+	if (!isServerSideRendered()) {		// TODO: Make this only visible when developers are logged in.
+		import('react-dom').then((ReactDOM) => {
+			import('@axe-core/react').then((axe) => {
+				axe.default(React, ReactDOM, 1000, {})
+			})
+		})
+	}
+
 	const getLayout = Component.getLayout || ((page) => page)
 
 	return (
