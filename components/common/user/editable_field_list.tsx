3c4450ad33d8d173822d1ff430d3ad6c5f68c4ee
Dániel B. Papp <chef-danny-d@users.noreply.github.com>
2023-04-18 15:38:35 +0000

    Automated formatting changes

--- components/common/user/editable_field_list.tsx
+++ components/common/user/editable_field_list.tsx
@@ -1,84 +1,93 @@
-import React from 'react';
-import { Input, InputProps } from '@/common/forms/inputs/input/input';
-import { InstructorProfile } from '@/types/index';
-import { Button } from '@/common/button/button';
+import React from 'react'
+import { Input, InputProps } from '@/common/forms/inputs/input/input'
+import { InstructorProfile } from '@/types/index'
+import { Button } from '@/common/button/button'
 
 interface EditableFieldListProps {
-	listData: Array<string> | [];
-	handleUpdate: React.Dispatch<React.SetStateAction<InstructorProfile>>;
-	mutableData: InstructorProfile;
-	isEditing: boolean;
-	persistentData: InstructorProfile;
-	inputDetails: Omit<InputProps, 'onChange'>;
+	listData: Array<string> | []
+	handleUpdate: React.Dispatch<React.SetStateAction<InstructorProfile>>
+	mutableData: InstructorProfile
+	isEditing: boolean
+	persistentData: InstructorProfile
+	inputDetails: Omit<InputProps, 'onChange'>
 }
 
 export const EditableFieldList = ({
-																		listData,
-																		handleUpdate,
-																		mutableData,
-																		persistentData,
-																		inputDetails,
-																		isEditing,
-																	}: EditableFieldListProps) => {
-	const [addEntryData, setAddEntryData] = React.useState(null);
-	const [isAddingEntry, setIsAddingEntry] = React.useState(false);
+	listData,
+	handleUpdate,
+	mutableData,
+	persistentData,
+	inputDetails,
+	isEditing,
+}: EditableFieldListProps) => {
+	const [addEntryData, setAddEntryData] = React.useState(null)
+	const [isAddingEntry, setIsAddingEntry] = React.useState(false)
 	return (
-		<ul className='ml-3 list-none m-0'>
+		<ul className="ml-3 list-none m-0">
 			{listData.length > 0
 				? listData.map((item, index) => {
-					if (isEditing) {
-						return (
-							<li
-								key={index}
-								className='flex justify-between gap-1'
-							>
-								<Input
-									{...inputDetails}
-									defaultValue={item}
-									onChange={(e) =>
-										handleUpdate((prev) => ({
-											...prev,
-											[inputDetails.name]:
-												mutableData[
-													inputDetails.name
+						if (isEditing) {
+							return (
+								<li
+									key={index}
+									className="flex justify-between gap-1"
+								>
+									<Input
+										{...inputDetails}
+										defaultValue={item}
+										onChange={(e) =>
+											handleUpdate((prev) => ({
+												...prev,
+												[inputDetails.name]:
+													mutableData[
+														inputDetails.name
 													].map((item, i) => {
-													if (i === index) {
-														return e;
-													}
-													return item;
-												}),
-										}))
-									}
-								/>
-								<Button className='rounded-md' size='small' onClick={() => {
-									handleUpdate((prev) => ({
-										...prev,
-										[inputDetails.name]:
-											mutableData[
-												inputDetails.name
-												].filter((item, i) => {
-												return i !== index;
-											}),
-									}));
-								}}>
-									X
-								</Button>
-							</li>
-						);
-					} else {
-						return (
-							<li key={index} className='sans my-1'>
-								{item}
-							</li>
-						);
-					}
-				})
+														if (i === index) {
+															return e
+														}
+														return item
+													}),
+											}))
+										}
+									/>
+									<Button
+										className="rounded-md"
+										size="small"
+										onClick={() => {
+											handleUpdate((prev) => ({
+												...prev,
+												[inputDetails.name]:
+													mutableData[
+														inputDetails.name
+													].filter((item, i) => {
+														return i !== index
+													}),
+											}))
+										}}
+									>
+										X
+									</Button>
+								</li>
+							)
+						} else {
+							return (
+								<li key={index} className="sans my-1">
+									{item}
+								</li>
+							)
+						}
+				  })
 				: null}
-			{isEditing &&
+			{isEditing && (
 				<>
 					{isAddingEntry && (
-						<div className='flex gap-1'>
-							<Input label={null} name={inputDetails.name} onChange={(e) => setAddEntryData(e)} type={'text'} />
+						<div className="flex gap-1">
+							<Input
+								label={null}
+								name={inputDetails.name}
+								onChange={(e) => setAddEntryData(e)}
+								type={'text'}
+							/>
 							<Button
 								onClick={() => {
 									handleUpdate((prev) => ({
@@ -87,27 +96,36 @@ export const EditableFieldList = ({
 											...mutableData[inputDetails.name],
 											addEntryData,
 										],
-									}));
-									setIsAddingEntry(false);
-									setAddEntryData(null);
+									}))
+									setIsAddingEntry(false)
+									setAddEntryData(null)
 								}}
 								size="small"
-							>Add</Button>
+							>
+								Add
+							</Button>
 							<Button
 								onClick={() => {
-									setIsAddingEntry(false);
-									setAddEntryData(null);
+									setIsAddingEntry(false)
+									setAddEntryData(null)
 								}}
 								size="small"
-							>Cancel</Button>
-
+							>
+								Cancel
+							</Button>
 						</div>
 					)}
-					<Button className='sans my-1' onClick={() => {
-						setIsAddingEntry(true);
-					}} size="small">Add entry</Button>
+					<Button
+						className="sans my-1"
+						onClick={() => {
+							setIsAddingEntry(true)
+						}}
+						size="small"
+					>
+						Add entry
+					</Button>
 				</>
-			}
+			)}
 		</ul>
-	);
-};
+	)
+}
