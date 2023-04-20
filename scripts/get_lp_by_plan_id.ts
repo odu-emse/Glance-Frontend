698a8f5e18047ad47b66a503c5443f6fd25a81f6
Dániel B. Papp <dpapp@odu.edu>
2023-04-20 14:48:08 -0400

    feat(LP): created reusable graphql query

---
+++ scripts/get_lp_by_plan_id.ts
@@ -0,0 +1,51 @@
+import { gql } from 'graphql-request';
+
+export default function getLPbyPlanID(planID) {
+	return gql`
+      query GetLPs {
+          learningPath(planID: "${planID}") {
+              id
+              paths{
+                  id
+                  createdAt
+                  status
+                  learningOutcomes
+                  hoursSatisfies
+                  course{
+                      id
+                      name
+                      carnegieHours
+                      required
+                      sections{
+                          id
+                          name
+                          collections{
+                              id
+                              name
+                              modules{
+                                  id
+                                  moduleName
+                                  moduleNumber
+                                  description
+                                  intro
+                                  keywords
+                                  objectives
+                                  parentModules{
+                                      id
+                                      moduleName
+                                      moduleNumber
+                                  }
+                                  subModules{
+                                      id
+                                      moduleName
+                                      moduleNumber
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+	`
+}
