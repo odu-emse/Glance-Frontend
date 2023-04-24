b5047e3601a9bc9316bb30e1e528bddafc9f4b26
Dániel B. Papp <dpapp@odu.edu>
2023-04-24 12:54:53 -0400

    feat(LP): Seperated layout into components

--- pages/plan/[plan_id]/paths/[path_id]/index.tsx
+++ pages/plan/[plan_id]/paths/[path_id]/index.tsx
@@ -45,217 +45,205 @@ function PathIndexPage() {
 	const LP = data.learningPath[0].paths[0];
 
 	return (
-		<section className="stdcontainer">
-			<div className="flex gap-4 items-center justify-start my-4">
+		<section className='stdcontainer'>
+			<div className='flex gap-4 items-center justify-start my-4'>
 				<Button onClick={() => router.back()}>Back</Button>
 				<h1>Path to {LP.course.name}</h1>
 			</div>
-			<div className="flex gap-1 items-start">
-				<div className="flex-none w-2/3">
-					<ul className="list-none m-0">
-						{LP.course.sections.map((section, index) => {
-							return (
-								<li key={index}>
-									<div
-										className="flex gap-1 items-center justify-start mb-2.5 cursor-pointer"
-										onClick={() =>
-											setCollapseSection(
-												collapseSection === index
-													? null
-													: index
-											)
-										}
-										ref={sectionRef}
-									>
-										{collapseSection !== index ? (
-											<FaPlus />
-										) : (
-											<FaMinus />
-										)}
-										<h3 className="cursor-pointer mb-0">
-											{section.name}
-										</h3>
-									</div>
-									{collapseSection === index && (
-										<ul className="list-none">
-											{section.collections.map(
-												(collection, index) => {
-													return (
-														<li key={index}>
-															<h4>
-																{
-																	collection.name
-																}
-															</h4>
-															<ul className="list-none divide-y">
-																{collection.modules.map(
-																	(
-																		module,
-																		index
-																	) => {
-																		return (
-																			<li
-																				key={
-																					index
-																				}
-																			>
-																				<div className="flex gap-2 items-center justify-start">
-																					<h5>
-																						{
-																							module.moduleName
-																						}
-																					</h5>
-																					<h6>
-																						{
-																							module.moduleNumber
-																						}
-																					</h6>
-																				</div>
-																				<p className="sans">
-																					{
-																						module.description
-																					}
-																				</p>
-																				<div className="my-3">
-																					{module
-																						.parentModules
-																						.length >
-																						0 && (
-																						<>
-																							<h5 className="sans">
-																								Recommended
-																								to
-																								complete
-																								first
-																							</h5>
-																							<div className="flex gap-1">
-																								{module.parentModules.map(
-																									(
-																										parentModule,
-																										index
-																									) => {
-																										return (
-																											<div
-																												className="flex gap-2 bg-wgray rounded px-2 py-1 sans text-sm"
-																												key={
-																													index
-																												}
-																											>
-																												<h5>
-																													{
-																														parentModule.moduleName
-																													}
-																												</h5>
-																												<h6>
-																													{
-																														parentModule.moduleNumber
-																													}
-																												</h6>
-																											</div>
-																										)
-																									}
-																								)}
-																							</div>
-																						</>
-																					)}
-																				</div>
-																				<div className="my-3">
-																					{module
-																						.subModules
-																						.length >
-																						0 && (
-																						<>
-																							<h5 className="sans">
-																								Possible
-																								next
-																								steps...
-																							</h5>
-																							<div className="flex gap-1">
-																								{module.subModules.map(
-																									(
-																										subModule,
-																										index
-																									) => {
-																										return (
-																											<div
-																												className="flex gap-2 bg-wgray rounded px-2 py-1 sans text-sm"
-																												key={
-																													index
-																												}
-																											>
-																												<h5>
-																													{
-																														subModule.moduleName
-																													}
-																												</h5>
-																												<h6>
-																													{
-																														subModule.moduleNumber
-																													}
-																												</h6>
-																											</div>
-																										)
-																									}
-																								)}
-																							</div>
-																						</>
-																					)}
-																				</div>
-																			</li>
-																		)
-																	}
-																)}
-															</ul>
-														</li>
-													)
-												}
-											)}
-										</ul>
-									)}
-								</li>
-							)
-						})}
+			<div className='flex gap-1 items-start'>
+				<div className='flex-none w-2/3'>
+					<ul className='list-none m-0'>
+						{LP.course.sections.map((section, index) =>
+							<SectionCard section={section} key={index} collapseIndex={collapseSection} sectionIndex={index} handleCollapse={setCollapseSection} sectionPointer={sectionRef} />,
+						)}
 					</ul>
 				</div>
-				<aside className="flex-1 w-1/3 flex flex-col gap-3 border border-r-0 border-gray-300 relative h-screen overflow-y-scroll bg-white transition-all p-3">
-					<Button className="w-fit mx-auto">
-						Edit Learning Path
-					</Button>
-					<div className="flex gap-1 items-center">
-						<h4>Hours satisfied:</h4>
-						<time
-							className={`${
-								LP.hoursSatisfies < LP.course.carnegieHours
-									? 'text-red-500'
-									: 'text-royalblue'
-							}`}
-						>
-							{LP.hoursSatisfies}
-						</time>
-					</div>
-					<div className="flex gap-1 items-center">
-						<h4>Status:</h4>
-						<span className="text-sm py-1 rounded-md bg-wgray sans font-semibold px-2 inline-block w-fit">
-							{LP.status}
-						</span>
-					</div>
-					<div className="">
-						<h3>Learning outcomes</h3>
-						<ul className="mt-0">
-							{LP.learningOutcomes.map((lo, index) => {
-								return (
-									<li key={index} className="sans">
-										{lo}
-									</li>
-								)
-							})}
-						</ul>
-					</div>
-				</aside>
+				<LPSidebar LP={LP} />
 			</div>
 		</section>
 	);
 }
+
+const SectionCard = ({ section, sectionIndex, collapseIndex, handleCollapse, sectionPointer }: {
+	section: SectionPath,
+	sectionIndex: number,
+	collapseIndex: number,
+	handleCollapse: React.Dispatch<
+		React.SetStateAction<number | null>
+	>
+	sectionPointer: React.MutableRefObject<HTMLDivElement>
+}) => {
+	return (
+		<li key={sectionIndex}>
+			<div
+				className='flex gap-1 items-center justify-start mb-2.5 cursor-pointer'
+				onClick={() =>
+					handleCollapse(
+						collapseIndex === sectionIndex
+							? null
+							: sectionIndex,
+					)
+				}
+				ref={sectionPointer}
+			>
+				{collapseIndex !== sectionIndex ? (
+					<FaPlus />
+				) : (
+					<FaMinus />
+				)}
+				<h3 className='cursor-pointer mb-0'>
+					{section.name}
+				</h3>
+			</div>
+			{collapseIndex === sectionIndex && (
+				<ul className='list-none'>
+					{section.collections.map((collection, index) => {
+						return <CollectionCard collection={collection} key={index} />;
+					})
+					}
+				</ul>
+			)}
+		</li>
+	);
+};
+
+const CollectionCard = ({ collection }: {
+	collection: CollectionPath
+}) => {
+	return (
+		<li>
+			<h4>
+				{
+					collection.name
+				}
+			</h4>
+			<ul className='list-none divide-y'>
+				{collection.modules.map((module, index) => <ModuleCard key={index} module={module} />)}
+			</ul>
+		</li>
+	);
+};
+
+const ModuleCard = ({ module }: {
+	module: Module
+}) => {
+	return (
+		<li
+		>
+			<div className='flex gap-2 items-center justify-start'>
+				<Link href={`/modules/${module.id}`}>
+					<a>{
+						`${module.moduleName} - ${module.moduleNumber}`
+					}</a>
+				</Link>
+			</div>
+			<p className='sans mt-0'>
+				{
+					module.intro
+				}
+			</p>
+			<h5 className='sans'>
+				Description
+			</h5>
+			<p className='sans mt-0'>
+				{
+					module.description
+				}
+			</p>
+			<h5 className='sans'>
+				Objectives
+			</h5>
+			<ul className="mt-0">
+				{module.objectives.map((item, index) => <li className="sans" key={index}>{item}</li>)}
+			</ul>
+			<ModuleTagList title={'Topics covered'} listData={module.keywords} elementName={null} elementLink={null} />
+			<ModuleTagList title={'Recommended to complete first'} listData={module.parentModules} elementName={'moduleName'} elementLink={'id'} />
+			<ModuleTagList title={'Possible next steps...'} listData={module.subModules} elementName={'moduleName'} elementLink={'id'} />
+		</li>
+	);
+};
+
+const ModuleTagList = ({ title, listData, elementName, elementLink }: {
+	title: string,
+	listData: Array<Module | string>,
+	elementName: string,
+	elementLink: string
+}) => {
+	if (listData.length === 0) return null;
+	return (
+		<div className='my-3'>
+			<h5 className='sans'>
+				{title}
+			</h5>
+			<div className='flex gap-1'>
+				{
+					listData.map((item, index) => {
+						if(typeof item === 'string'){
+							return (
+								<span className='flex gap-2 bg-wgray rounded px-2 py-1 sans text-sm w-fit' key={index}>
+									<h6>
+										{item}
+									</h6>
+								</span>
+							);
+						}
+						return (
+							<Link href={`/modules/${item[elementLink]}`} key={index}>
+								<a className='flex gap-2 bg-wgray rounded border-2 border-wgray px-2 py-1 sans text-sm w-fit no-underline cursor-pointer hover:bg-white hover:border-wgray transition-all'>
+									<h6 className="hover:text-royalblue">
+										{item[elementName]}
+									</h6>
+								</a>
+							</Link>
+						);
+					})
+				}
+			</div>
+		</div>
+	);
+};
+
+const LPSidebar = ({ LP } : {
+	LP: Path
+}) => {
+	return (
+		<aside className='flex-1 w-1/3 flex flex-col gap-3 border border-r-0 border-gray-300 relative h-screen overflow-y-scroll bg-white transition-all p-3'>
+			<Button className='w-fit mx-auto'>
+				Edit Learning Path
+			</Button>
+			<div className='flex gap-1 items-center'>
+				<h4>Hours satisfied:</h4>
+				<time
+					className={`${
+						LP.hoursSatisfies < LP.course.carnegieHours
+							? 'text-red-500'
+							: 'text-royalblue'
+					}`}
+				>
+					{LP.hoursSatisfies}
+				</time>
+			</div>
+			<div className='flex gap-1 items-center'>
+				<h4>Status:</h4>
+				<span className='text-sm py-1 rounded-md bg-wgray sans font-semibold px-2 inline-block w-fit'>
+							{LP.status}
+						</span>
+			</div>
+			<div className=''>
+				<h3>Learning outcomes</h3>
+				<ul className='mt-0'>
+					{LP.learningOutcomes.map((lo, index) => {
+						return (
+							<li key={index} className='sans'>
+								{lo}
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</aside>
+	);
+};
 
 PathIndexPage.getLayout = function getLayout(page) {
 	return <Layout>{page}</Layout>;
