ade3554a469bd4a8233f3939e7beaa10a1e416f7
Dániel B. Papp <dpapp@odu.edu>
2023-04-25 13:18:03 -0400

    refactor(LP): moved components to separate files

---
+++ components/common/learning_path/module/module_card.tsx
@@ -0,0 +1,47 @@
+import { Module } from '@/types/index';
+import Link from 'next/link';
+import React from 'react';
+import ModuleTagList from '@/common/learning_path/module/module_tag_list';
+
+const ModuleCard = ({ module }: { module: Module }) => {
+	return (
+		<li>
+			<div className="flex gap-2 items-center justify-start">
+				<Link href={`/modules/${module.id}`}>
+					<a>{`${module.moduleName} - ${module.moduleNumber}`}</a>
+				</Link>
+			</div>
+			<p className="sans mt-0">{module.intro}</p>
+			<h5 className="sans">Description</h5>
+			<p className="sans mt-0">{module.description}</p>
+			<h5 className="sans">Objectives</h5>
+			<ul className="mt-0">
+				{module.objectives.map((item, index) => (
+					<li className="sans" key={index}>
+						{item}
+					</li>
+				))}
+			</ul>
+			<ModuleTagList
+				title={'Topics covered'}
+				listData={module.keywords}
+				elementName={null}
+				elementLink={null}
+			/>
+			<ModuleTagList
+				title={'Recommended to complete first'}
+				listData={module.parentModules}
+				elementName={'moduleName'}
+				elementLink={'id'}
+			/>
+			<ModuleTagList
+				title={'Possible next steps...'}
+				listData={module.subModules}
+				elementName={'moduleName'}
+				elementLink={'id'}
+			/>
+		</li>
+	)
+}
+
+export default ModuleCard;
---
+++ components/common/learning_path/section/section_card.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Module, SectionPath } from '@/types/index';
+import SectionCardTitle from '@/common/learning_path/section/section_card_title';
+import CollectionCard from '@/common/learning_path/collection/collection_card';
+
+const SectionCard = ({
+											 section,
+											 sectionIndex,
+											 collapseIndex,
+											 handleCollapse,
+											 sectionPointer,
+											 editMode,
+											 sectionsData
+										 }: {
+	section: SectionPath,
+	sectionIndex: number,
+	collapseIndex: number,
+	handleCollapse: React.Dispatch<React.SetStateAction<number | null>>,
+	sectionPointer: React.MutableRefObject<HTMLDivElement>,
+	editMode: boolean,
+	sectionsData: Array<Module>
+}) => {
+	if(editMode){
+		return (
+			<li key={sectionIndex}>
+				<SectionCardTitle sectionPointer={sectionPointer} sectionIndex={sectionIndex} section={section} collapseIndex={collapseIndex} handleCollapse={handleCollapse} editMode={editMode} data={sectionsData} />
+				<ul className="list-none">
+					{section.collections.map((collection, index) => {
+						return (
+							<CollectionCard
+								collection={collection}
+								key={index}
+							/>
+						)
+					})}
+				</ul>
+			</li>
+		)
+	}
+	return (
+		<li key={sectionIndex}>
+			<SectionCardTitle sectionPointer={sectionPointer} sectionIndex={sectionIndex} section={section} collapseIndex={collapseIndex} handleCollapse={handleCollapse} editMode={editMode} data={sectionsData} />
+			{collapseIndex === sectionIndex && (
+				<ul className="list-none">
+					{section.collections.map((collection, index) => {
+						return (
+							<CollectionCard
+								collection={collection}
+								key={index}
+							/>
+						)
+					})}
+				</ul>
+			)}
+		</li>
+	)
+}
+
+export default SectionCard;
