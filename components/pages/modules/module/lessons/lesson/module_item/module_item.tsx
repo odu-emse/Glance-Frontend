ca976b95e16408f56c2ba312c30c018f2b1e29f6
Dániel B. Papp <chef-danny-d@users.noreply.github.com>
2023-04-27 18:34:07 +0000

    Automated formatting changes

--- components/pages/modules/module/lessons/lesson/module_item/module_item.tsx
+++ components/pages/modules/module/lessons/lesson/module_item/module_item.tsx
@@ -1,31 +1,44 @@
 import Link from 'next/link'
 import { HiChevronRight } from 'react-icons/hi'
 import { Module } from '@/types/graphql'
-import React from 'react';
+import React from 'react'
 
-export const ModuleItem = ({ data, role, expanded, handleExpansion, selected }: ModuleItemProps) => {
+export const ModuleItem = ({
+	data,
+	role,
+	expanded,
+	handleExpansion,
+	selected,
+}: ModuleItemProps) => {
 	// Use the link below once DB schema is updated
 	// const link = `/modules/${data.id}/sections/${data?.headSection}/lessons/${data?.sections[data?.headSection]?.headLesson}`
 	return (
-			<div className={`px-5 py-4 rounded-md shadow cursor-pointer flex justify-center items-center w-full border-2 ${selected ? "border-royalblue" : "border-transparent"}`} >
-				<div className="flex grow flex-col gap-1" onClick={() => handleExpansion(!expanded)}>
-					<div
-						className="flex flex-row gap-1 sans text-sm sans uppercase items-center"
-					>
-						<p className="p-0 m-0">
-							SECTION {data.collections[0].section.sectionNumber}
-						</p>
-						<span>/</span>
-						<p className="p-0 m-0">
-							MODULE {data.prefix && data.prefix}
-							{data.number}
-						</p>
-					</div>
-					<div>
-						<h2 className="sans leading-5 text-royalblue font-semibold">{data.name}</h2>
-					</div>
+		<div
+			className={`px-5 py-4 rounded-md shadow cursor-pointer flex justify-center items-center w-full border-2 ${
+				selected ? 'border-royalblue' : 'border-transparent'
+			}`}
+		>
+			<div
+				className="flex grow flex-col gap-1"
+				onClick={() => handleExpansion(!expanded)}
+			>
+				<div className="flex flex-row gap-1 sans text-sm sans uppercase items-center">
+					<p className="p-0 m-0">
+						SECTION {data.collections[0].section.sectionNumber}
+					</p>
+					<span>/</span>
+					<p className="p-0 m-0">
+						MODULE {data.prefix && data.prefix}
+						{data.number}
+					</p>
 				</div>
-				<Link href={`/modules/${data.id}`} passHref>
+				<div>
+					<h2 className="sans leading-5 text-royalblue font-semibold">
+						{data.name}
+					</h2>
+				</div>
+			</div>
+			<Link href={`/modules/${data.id}`} passHref>
 				<a className="block">
 					<span
 						className="w-12 h-12 bg-royalblue text-white rounded-full flex justify-center items-center"
@@ -37,14 +50,14 @@ export const ModuleItem = ({ data, role, expanded, handleExpansion, selected }: 
 					</span>
 				</a>
 			</Link>
-			</div>
+		</div>
 	)
 }
 
 export type ModuleItemProps = {
-	data: Module,
-	role: String,
-	expanded: boolean,
-	handleExpansion: React.Dispatch<React.SetStateAction<boolean>>,
+	data: Module
+	role: String
+	expanded: boolean
+	handleExpansion: React.Dispatch<React.SetStateAction<boolean>>
 	selected: boolean
 }
