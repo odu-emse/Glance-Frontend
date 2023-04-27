4c62d6a1adb75f8c1342ac66cacd7d2fe484ea0a
Dániel B. Papp <chef-danny-d@users.noreply.github.com>
2023-04-27 16:26:44 +0000

    Automated formatting changes

--- components/pages/communities/ThreadCreation/thread_page.stories.tsx
+++ components/pages/communities/ThreadCreation/thread_page.stories.tsx
@@ -3,12 +3,11 @@ import * as React from 'react'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import { ThreadCreation, ThreadCreationProps } from './thread_page'
 
-
 export default {
 	title: 'Pages/Communities/Thread/Thread Creation',
 	component: ThreadCreation,
 	argTypes: {},
-} 
+}
 
 const Template: ComponentStory<typeof ThreadCreation> = (
 	args: ThreadCreationProps
