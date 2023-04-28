776d8aa7a23bccbc6dc2a0e3e2e0f124e4fad373
Joel DeSante <joeldesante@gmail.com>
2023-04-28 13:38:42 -0400

    Clear up issues with 404 page back button

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,21 +7,13 @@ import { useEffect } from 'react'
 const NotFound = () => {
 	const router = useRouter()
 
-	const handleReturn = () => {
-		if (router.back) {
-			router.back()
-		} else {
-			router.push('/modules')
-		}
-	}
-
 	return (
 		<div className="mx-auto w-full flex flex-col items-center justify-center h-screen">
 			<h1>404</h1>
 			<p>The resource was not found</p>
 			<Link className="underline text-blue-500" href="/">
-				<Button onClick={handleReturn}>
-					{router.back ? 'Back' : 'Return Home'}
+				<Button onClick={() => router.back()}>
+					Back
 				</Button>
 			</Link>
 			<DynamicVersion backendVersion={'0.3.0'} isLight={false} />
