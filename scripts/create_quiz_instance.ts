8ed45674115bcc5e17c7394738525f2bbb396fb0
Dániel B. Papp <dpapp@odu.edu>
2023-05-03 14:44:09 -0400

    feat(quiz): created data queries

---
+++ scripts/create_quiz_instance.ts
@@ -0,0 +1,17 @@
+import { gql } from 'graphql-request';
+
+export const createQuizInstance = gql`
+	query FetchQuizInstance($id: ID!) {
+		quizInstance(args: { id: $id }) {
+			id
+			questions {
+				text
+				answers {
+					id
+					text
+					correct
+				}
+			}
+		}
+	}
+`
---
+++ scripts/get_quiz_by_id.ts
@@ -0,0 +1,20 @@
+import { gql } from 'graphql-request';
+
+export const getQuizById =  gql`
+	query getQuizById($quizID: ID!) {
+		quiz(args: {
+			id: $quizID,
+		}){
+			numQuestions
+			id
+			totalPoints
+			dueAt
+			timeLimit
+			minScore
+			instances{
+				id
+			}
+			instructions
+		}
+	}
+`
