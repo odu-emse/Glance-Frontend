5f9729352c58e546e63cd351884e2918ce4fa934
Joel DeSante <joeldesante@gmail.com>
2023-05-08 15:05:49 -0400

    Clean up the primary sidebar

---
+++ components/common/pages/sidebar/collapse_button/collapse_button.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+
+export type CollapseButtonProps = {
+	open: boolean
+}
+
+export const CollapseButton: React.FC<CollapseButtonProps> = ({
+	open
+}) => {
+	return (
+		<button
+			className="p-2 rounded-md text-red border bg-royalblue"
+		>
+			{open ? (
+				<IoIosArrowBack color="white" />
+			) : (
+				<IoIosArrowForward color="white" />
+			)}
+		</button>
+	)
+}
