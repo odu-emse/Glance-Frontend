384f75cffa7e64a23a842f721f45e69b479025ac
Dániel B. Papp <dpapp@odu.edu>
2023-05-11 15:57:54 -0400

    refactor(sidebar): added link to messages

--- components/common/pages/sidebar/sidebar.tsx
+++ components/common/pages/sidebar/sidebar.tsx
@@ -5,6 +5,7 @@ import { PlanOfStudy } from '../../svg/plan_of_study'
 import { Modules } from '../../svg/modules'
 import { Communities } from '../../svg/communities'
 import { GradeBook } from '../../svg/gradebook'
+import { ChatBubble } from '@/common/svg/chat_bubble';
 
 type IconType = React.ReactNode
 
@@ -52,6 +53,13 @@ export const Sidebar: React.FC<SidebarProps> = ({
 						collapsed={!open}
 						value="COMMUNITIES"
 						href="/communities"
+						icon={''}
+					/>
+					<SidebarItem
+						svgIcon={<ChatBubble width={20} height={20} />}
+						collapsed={!open}
+						value="MESSAGES"
+						href="/messages"
 						icon={''}
 					/>
 					<SidebarItem
