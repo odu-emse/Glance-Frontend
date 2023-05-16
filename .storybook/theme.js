0cb7b08a28aba75d975d71ad38983483de6d4d1a
Joel DeSante <joeldesante@gmail.com>
2023-05-16 15:01:19 -0400

    Update storybook theme

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+    base: 'light',
+    brandTitle: 'Glance UI',
+    brandUrl: 'https://odu-emse.github.io/Glance-Frontend',
+    brandImage: 'https://raw.githubusercontent.com/odu-emse/Glance-Frontend/dfcceb573bbd40592e48d87f660489c66866838f/public/images/GLANCE_1.png',
+    brandTarget: '_self',
+})
