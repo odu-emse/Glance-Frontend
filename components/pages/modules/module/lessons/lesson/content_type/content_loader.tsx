94a4ab5d7e86e51a085bd4f6025f63e21d7a7e6a
Joel DeSante <joeldesante@gmail.com>
2023-06-28 14:58:26 -0400

    Improved error message when type is undefined

--- components/pages/modules/module/lessons/lesson/content_type/content_loader.tsx
+++ components/pages/modules/module/lessons/lesson/content_type/content_loader.tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic'
 
 export const ContentLoader = ({ type, data }: {type: any | undefined, data: any}) => {
 	console.log('ContentLoader:', type, data)
-	if(type === undefined) return <p>Content in Undefined</p>
+	if(type === undefined) return <pre>Error: Could not load content. Content `type` is undefined. Please contact the system adminsitrator to resolve this issue.</pre>
 
 	const Content = dynamic(() => import(`./${type.toLowerCase()}`), {
 		ssr: false,
